377df5fcea79e0690c38839572ba32585aed92bd
Hideaki SAWADA <chuno@live.jp>
2014-09-09 13:28:18 +0900

    Create settings.php

---
+++ ja/settings.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Hideaki SAWADA <chuno@live.jp>
+ */
+
+$lang['tpl_dir'] = 'メディアマネージャー内のテンプレート用サブディレクトリ';
+$lang['showexportbutton']      = 'ODT 出力ボタンを表示';

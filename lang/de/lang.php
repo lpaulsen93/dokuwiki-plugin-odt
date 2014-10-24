aa09554a036bf195d3bcab1be2398e52282ee20a
Florian Lamml <info@florian-lamml.de>
2014-10-24 08:40:06 +0200

    translation update

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -1,10 +1,12 @@
 <?php
 
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-       
-// export button
-$lang['view'] = 'Ins Open-Document-Format exportieren';
-$lang['export_odt_button'] = "ODT exportieren";
-
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Florian Lamml <info@florian-lamml.de>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['view']                  = 'Ins Open-Document-Format exportieren';
+$lang['export_odt_button']     = 'ODT exportieren';
+$lang['tpl_not_found']         = 'WARNUNG: Die ODT Vorlage "%s" konnte im Vorlagenverzeichnis "%s" nicht gefunden werden. Es wird die Standardvorlage verwendet.';

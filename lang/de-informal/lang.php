371915c2456f2daaf73115051e8f33008298ea9e
Florian Lamml <info@florian-lamml.de>
2014-10-24 09:25:36 +0200

    translation update

---
+++ lang/de-informal/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Florian Lamml <info@florian-lamml.de>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['view']                  = 'Die Seite im Open Document Format exportieren';
+$lang['export_odt_button']     = 'ODT exportieren';
+$lang['tpl_not_found']         = 'WARNUNG: Die ODT Vorlage "%s" konnte im Vorlagenverzeichnis "%s" nicht gefunden werden. Es wird die Standardvorlage verwendet.';
---
+++ lang/de-informal/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Florian Lamml <info@florian-lamml.de>
+ */
+$lang['tpl_dir']               = 'Unterverzeichnis für Vorlagen im Media Manager';
+$lang['showexportbutton']      = 'Den ODT Export Button anzeigen';

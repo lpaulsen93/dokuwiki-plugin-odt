0f11b57c116a88c68ad0c61a9b4b0b449b93c2e5
NicolasFriedli <nicolas@theologique.ch>
2014-11-13 09:20:37 +0100

    translation update

---
+++ lang/fr/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author NicolasFriedli <nicolas@theologique.ch>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'gauche à droite';
+$lang['view']                  = 'Exporter la page en format Open Document';
+$lang['export_odt_button']     = 'exportation ODT';
+$lang['tpl_not_found']         = 'ATTENTION: le modèle ODT "%s" est introuvable dans le répertoire "%s". Le modèle par défaut est utilisé.';

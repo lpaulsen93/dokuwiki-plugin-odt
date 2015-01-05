a02735eb136ec992160edcf723d3609a4a38c2e9
Wouter Wijsman <wwijsman@live.nl>
2015-01-05 14:55:37 +0100

    translation update

---
+++ lang/nl/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Wouter Wijsman <wwijsman@live.nl>
+ */
+$lang['showexportbutton']      = 'Geef ODT Exporteer Knop weer';

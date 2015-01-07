943d1d315e01b88c049ddadd11a774bb7cd3b666
Mijndert <mijndert@mijndertstuij.nl>
2015-01-07 11:40:05 +0100

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -4,7 +4,9 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Wouter Wijsman <wwijsman@live.nl>
+ * @author Mijndert <mijndert@mijndertstuij.nl>
  */
 $lang['encoding']              = 'utf-8';
 $lang['view']                  = 'Exporteer pagina naar Open Document formaat';
 $lang['export_odt_button']     = 'ODT exporteren';
+$lang['tpl_not_found']         = 'WAARSCHUWING: de ODT template "%2" is niet gevonden in de templates map. De standaard template wordt gebruikt.';
--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -4,5 +4,7 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Wouter Wijsman <wwijsman@live.nl>
+ * @author Mijndert <mijndert@mijndertstuij.nl>
  */
+$lang['tpl_dir']               = 'Sub-map voor de templates in de media manager';
 $lang['showexportbutton']      = 'Geef ODT Exporteer Knop weer';

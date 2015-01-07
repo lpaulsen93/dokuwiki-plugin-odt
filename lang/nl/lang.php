16fd086d6ccb6c8eee217eb1e154bcf1b1530aec
Arthur Buijs <arthur@artietee.nl>
2015-01-07 21:05:38 +0100

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -4,7 +4,10 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author Wouter Wijsman <wwijsman@live.nl>
+ * @author Arthur Buijs <arthur@artietee.nl>
  */
 $lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
 $lang['view']                  = 'Exporteer pagina naar Open Document formaat';
 $lang['export_odt_button']     = 'ODT exporteren';
+$lang['tpl_not_found']         = 'WAARSCHUWING : het ODT-sjabloon "%s" is niet aanwezig in de directory voor sjablonen "%s". Het standaardsjabloon zal gebruikt worden.  ';

bbc75fbaed2f3b74da47d9a3923761ad089bba3d
Imre Nagy <crash2@freemail.hu>
2015-01-20 12:55:39 +0100

    translation update

---
+++ lang/hu/lang.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Imre Nagy <crash2@freemail.hu>
+ */
+$lang['encoding']              = 'utf-8';
+$lang['direction']             = 'ltr';
+$lang['view']                  = 'Oldal exportálása OpenDocument formátumba';
+$lang['export_odt_button']     = 'Exportálás ODT-be';
---
+++ lang/hu/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Imre Nagy <crash2@freemail.hu>
+ */
+$lang['showexportbutton']      = 'Exportálás ODT-be gomb megjelenítése';

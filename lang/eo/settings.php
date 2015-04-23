8f791f52d7c0845066647ae1ebcd28dd0aedc92c
Robert Bogenschneider <robog@gmx.de>
2015-04-23 08:20:22 +0200

    translation update

---
+++ lang/eo/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author Robert Bogenschneider <robog@gmx.de>
+ */
+$lang['tpl_dir']               = 'Subdosierujo por la ŝablonoj en la medi-administrilo';
+$lang['showexportbutton']      = 'Montri butonon ODT-Eksporto';

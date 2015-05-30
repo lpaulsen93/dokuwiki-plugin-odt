b7c3f75eb506ac5a8f4ef20c093bc0a4133b2b5b
LarsDW223 <lars_paulsen@web.de>
2015-05-30 10:49:52 +0200

    Added new helper class for unit conversion. Supported are units specified in XSL 'Extensible Stylesheet Language (XSL) Version 1.1'.

---
+++ _test/units.test.php
@@ -0,0 +1,97 @@
+<?php
+
+require_once DOKU_INC.'lib/plugins/odt/helper/units.php';
+
+/**
+ * Tests to ensure functionality of the units helper class.
+ *
+ * @group plugin_odt
+ * @group plugins
+ */
+class plugin_odt_units_test extends DokuWikiTest {
+    public function setUp() {
+        $this->pluginsEnabled[] = 'odt';
+        parent::setUp();
+    }
+
+    /**
+     * Ensure that stripDigits() strips all digits from the left correctly.
+     */
+    public function test_stripDigits() {
+        $units = new helper_plugin_odt_units ();
+
+        $this->assertEquals($units->stripDigits('1cm'), 'cm');
+        $this->assertEquals($units->stripDigits('12mm'), 'mm');
+        $this->assertEquals($units->stripDigits('123in'), 'in');
+        $this->assertEquals($units->stripDigits('1234pt'), 'pt');
+        $this->assertEquals($units->stripDigits('12345pc'), 'pc');
+        $this->assertEquals($units->stripDigits('123456px'), 'px');
+        $this->assertEquals($units->stripDigits('1234567em'), 'em');
+        $this->assertEquals($units->stripDigits('9m'), 'm');
+        $this->assertEquals($units->stripDigits('9km'), 'km');
+        $this->assertEquals($units->stripDigits('9mi'), 'mi');
+        $this->assertEquals($units->stripDigits('9ft'), 'ft');
+        $this->assertEquals($units->stripDigits('9ft23'), 'ft23');
+    }
+
+    /**
+     * Ensure that hasValidXSLUnit() recongizes XSL units correctly.
+     */
+    public function test_hasValidXSLUnit() {
+        $units = new helper_plugin_odt_units ();
+
+        $this->assertEquals($units->hasValidXSLUnit('1cm'), true);
+        $this->assertEquals($units->hasValidXSLUnit('12mm'), true);
+        $this->assertEquals($units->hasValidXSLUnit('123in'), true);
+        $this->assertEquals($units->hasValidXSLUnit('1234pt'), true);
+        $this->assertEquals($units->hasValidXSLUnit('12345pc'), true);
+        $this->assertEquals($units->hasValidXSLUnit('123456px'), true);
+        $this->assertEquals($units->hasValidXSLUnit('1234567em'), true);
+        $this->assertEquals($units->hasValidXSLUnit('9m'), false);
+        $this->assertEquals($units->hasValidXSLUnit('9km'), false);
+        $this->assertEquals($units->hasValidXSLUnit('9mi'), false);
+        $this->assertEquals($units->hasValidXSLUnit('9ft'), false);
+    }
+
+    /**
+     * Ensure that pixelToPoints function convert to points correctly.
+     */
+    public function test_pixelToPoints() {
+        $units = new helper_plugin_odt_units ();
+
+        // First with default values.
+        $this->assertEquals($units->pixelToPointsX('1px'), '0.8pt');
+        $this->assertEquals($units->pixelToPointsY('1px'), '1pt');
+
+        // Then with set values.
+        $units->setTwipsPerPixelX(32);
+        $units->setTwipsPerPixelY(40);
+
+        $this->assertEquals($units->getTwipsPerPixelX(), 32);
+        $this->assertEquals($units->getTwipsPerPixelY(), 40);
+        $this->assertEquals($units->pixelToPointsX('1px'), '1.6pt');
+        $this->assertEquals($units->pixelToPointsY('1px'), '2pt');
+    }
+
+    /**
+     * Ensure that conversion to points works correctly.
+     */
+    public function test_toPoints() {
+        $units = new helper_plugin_odt_units ();
+
+        // Set base values.
+        $units->setTwipsPerPixelX(16);
+        $units->setTwipsPerPixelY(20);
+        $units->setPixelPerEm(14);
+
+        $this->assertEquals($units->getPixelPerEm(), 14);
+
+        $this->assertEquals($units->toPoints('1cm'), '28.346456514353pt');
+        $this->assertEquals($units->toPoints('1mm'), '2.8346456514353pt');
+        $this->assertEquals($units->toPoints('1in'), '0.089605556pt');
+        $this->assertEquals($units->toPoints('1pc'), '12pt');
+        $this->assertEquals($units->toPoints('1px', 'x'), '0.8pt');
+        $this->assertEquals($units->toPoints('1px', 'y'), '1pt');
+        $this->assertEquals($units->toPoints('1em'), '14pt');
+    }
+}
---
+++ helper/units.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Simple helper class to work with units (e.g. 'px', 'pt', 'cm'...)
+ * 
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     LarsDW223
+ */
+
+// must be run within Dokuwiki
+if (!defined('DOKU_INC')) die();
+
+if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
+if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
+if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
+
+class helper_plugin_odt_units extends DokuWiki_Plugin {
+    // Measure units as defined in "Extensible Stylesheet Language (XSL) Version 1.1"
+    protected static $xsl_units = array('cm', 'mm', 'in', 'pt', 'pc', 'px', 'em');
+    protected static $twips_per_pixel_x = 16;
+    protected static $twips_per_pixel_y = 20;
+    protected static $twips_per_point   = 20;
+    protected static $point_in_cm = 0.035277778;
+    protected static $inch_in_cm = 2.54;
+    protected static $inch_in_pt = 0.089605556;
+    protected static $pc_in_cm = 0.423333336;
+    protected static $pc_in_pt = 12;
+    protected static $px_per_em = 14;
+
+    function getMethods() {
+        $result = array();
+        $result[] = array(
+                'name'   => 'getColorValue',
+                'desc'   => 'returns the color value for a given CSS color name. Returns "#000000" if the name is unknown',
+                'params' => array('name' => 'string'),
+                'return' => array('color value' => 'string'),                
+                );
+        return $result;
+    }
+
+    /**
+     * Strips of the leading digits from $value. So left over will be the unit only.
+     *
+     * @param $value The length value string, e.g. '1cm'.
+     * @return string The unit of $value, e.g. 'cm'
+     */
+    public function stripDigits ($value) {
+        return ltrim ($value, '.0123456789');
+    }
+
+    /**
+     * Gets only the digits from $value without the unit.
+     *
+     * @param $value The length value string, e.g. '1cm'.
+     * @return string The digits of $value, e.g. '1'
+     */
+    public function getDigits ($value) {
+        $digits = NULL;
+        $length = strlen ((string)$value);
+        for ($index = 0 ; $index < $length ; $index++ ) {
+            if ( is_numeric ($value [$index]) === false && $value [$index] != '.' ) {
+                break;
+            }
+            $digits .= $value [$index];
+        }
+        return $digits;
+    }
+
+    /**
+     * Checks if $unit is a valid XSL unit.
+     *
+     * @param $unit The unit string, e.g. 'cm'.
+     * @return boolean true if valid, false otherwise
+     */
+    public function isValidXSLUnit($unit) {
+        return in_array($unit, self::$xsl_units);
+    }
+
+    /**
+     * Checks if length value string $value has a valid XSL unit.
+     *
+     * @param $value The length value string, e.g. '1cm'.
+     * @return boolean true if valid, false otherwise
+     */
+    public function hasValidXSLUnit($value) {
+        return in_array($this->stripDigits((string)$value), self::$xsl_units);
+    }
+
+    /**
+     * Sets the pixel per em unit used for px to em conversion.
+     *
+     * @param $value The value to be set.
+     */
+    public function setPixelPerEm ($value) {
+        self::$px_per_em = $value;
+    }
+
+    /**
+     * Query the pixel per em unit.
+     *
+     * @return The current value.
+     */
+    public function getPixelPerEm () {
+        return self::$px_per_em;
+    }
+
+    /**
+     * Sets the twips per pixel (X axis) used for px to pt conversion.
+     *
+     * @param $value The value to be set.
+     */
+    public function setTwipsPerPixelX ($value) {
+        self::$twips_per_pixel_x = $value;
+    }
+
+    /**
+     * Sets the twips per pixel (Y axis) unit used for px to pt conversion.
+     *
+     * @param $value The value to be set.
+     */
+    public function setTwipsPerPixelY ($value) {
+        self::$twips_per_pixel_y = $value;
+    }
+
+    /**
+     * Query the twips per pixel (X axis) setting.
+     *
+     * @return The current value.
+     */
+    public function getTwipsPerPixelX () {
+        return self::$twips_per_pixel_x;
+    }
+
+    /**
+     * Query the twips per pixel (Y axis) setting.
+     *
+     * @return The current value.
+     */
+    public function getTwipsPerPixelY () {
+        return self::$twips_per_pixel_y;
+    }
+
+    /**
+     * Convert pixel (X axis) to points according to the current settings.
+     *
+     * @param $pixel String with pixel length value, e.g. '20px'
+     * @return The current value.
+     */
+    public function pixelToPointsX ($pixel) {
+        $pixel = $this->getDigits ((string)$pixel);
+        return ($pixel * self::$twips_per_pixel_x / self::$twips_per_point).'pt';
+    }
+
+    /**
+     * Convert pixel (Y axis) to points according to the current settings.
+     *
+     * @param $pixel String with pixel length value, e.g. '20px'
+     * @return The current value.
+     */
+    public function pixelToPointsY ($pixel) {
+        $pixel = $this->getDigits ((string)$pixel);
+        return ($pixel * self::$twips_per_pixel_y / self::$twips_per_point).'pt';
+    }
+
+    /**
+     * Convert length value with valid XSL unit to points.
+     *
+     * @param $value String with length value, e.g. '20px', '20cm'...
+     * @param $axis Is the value to be converted a value on the X or Y axis? Default is 'y'.
+     *        Only relevant for conversion from 'px' or 'em'.
+     * @return The current value.
+     */
+    public function toPoints ($value, $axis = 'y') {
+        $unit = $this->stripDigits ($value);
+        if ( $unit == 'pt' ) {
+            return $value;
+        }
+
+        if ( $this->isValidXSLUnit ($unit) === false  ) {
+            // Not a vlaid/supported unit. Return original value.
+            return $value;
+        }
+
+        $value = $this->getDigits ($value);
+        switch ($unit) {
+            case 'cm':
+                $value = ($value/self::$point_in_cm).'pt';
+            break;
+            case 'mm':
+                $value = ($value/(10 * self::$point_in_cm)).'pt';
+            break;
+            case 'in':
+                $value = ($value * self::$inch_in_pt).'pt';
+            break;
+            case 'pc':
+                $value = ($value * self::$pc_in_pt).'pt';
+            break;
+            case 'px':
+                if ( $axis == 'x' || $axis == 'X' ) {
+                    $value = $this->pixelToPointsX ($value);
+                } else {
+                    $value = $this->pixelToPointsY ($value);
+                }
+            break;
+            case 'em':
+                $value = $this->pixelToPointsY ($value * $this->getPixelPerEm());
+            break;
+        }
+        return $value;
+    }
+}
+?>

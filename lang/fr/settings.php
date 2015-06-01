57c9163e33cafd997cfdf27e5241d502f63b2eed
Schplurtz le Déboulonné <Schplurtz@laposte.net>
2015-06-01 19:40:06 +0200

    translation update

--- lang/fr/settings.php
+++ lang/fr/settings.php
@@ -4,10 +4,10 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author NicolasFriedli <nicolas@theologique.ch>
+ * @author Schplurtz le Déboulonné <Schplurtz@laposte.net>
  */
 $lang['tpl_dir']               = 'Sous-répertoire dédié aux modèles dans le gestionnaire de médias';
 $lang['showexportbutton']      = 'Afficher le bouton d\'exportation ODT';
-
+$lang['media_sel']             = 'Quel sélecteur @media utiliser pour l\'interrogation des propriétés CSS ?';
 $lang['template']              = 'Quel thème doit être utilisé pour présenter les ODT?';
 $lang['usestyles']             = 'Vous pouvez préciser une liste d\'extensions dont les fichiers <code>style.css</code> ou <code>screen.css</code> doivent être utilisés pour générer les ODT. Par défaut,  seuls les fichiers  <code>print.css</code> et <code>odt.css</code> sont utilisés.';
-

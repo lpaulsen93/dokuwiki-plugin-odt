30953c472868a121142bc4f75f667f7a47d1ba8f
Gerrit Uitslag <klapinklapin@gmail.com>
2015-06-03 22:00:25 +0200

    code reformatting

--- syntax.php
+++ syntax.php
@@ -17,21 +17,21 @@ class syntax_plugin_odt extends DokuWiki_Syntax_Plugin {
     /**
      * What kind of syntax are we?
      */
-    public function getType(){
+    public function getType() {
         return 'substition';
     }
 
     /**
      * What about paragraphs?
      */
-    public function getPType(){
+    public function getPType() {
         return 'normal';
     }
 
     /**
      * Where to sort in?
      */
-    public function getSort(){
+    public function getSort() {
         return 319; // Before image detection, which uses {{...}} and is 320
     }
 
@@ -41,37 +41,37 @@ class syntax_plugin_odt extends DokuWiki_Syntax_Plugin {
      * @param string $mode
      */
     public function connectTo($mode) {
-        $this->Lexer->addSpecialPattern('~~ODT~~',$mode,'plugin_odt');
-        $this->Lexer->addSpecialPattern('{{odt>.+?}}',$mode,'plugin_odt');
+        $this->Lexer->addSpecialPattern('~~ODT~~', $mode, 'plugin_odt');
+        $this->Lexer->addSpecialPattern('{{odt>.+?}}', $mode, 'plugin_odt');
     }
 
     /**
      * Handler to prepare matched data for the rendering process
      *
-     * @param   string       $match   The text matched by the patterns
-     * @param   int          $state   The lexer state for the match
-     * @param   int          $pos     The character position of the matched text
+     * @param   string $match The text matched by the patterns
+     * @param   int $state The lexer state for the match
+     * @param   int $pos The character position of the matched text
      * @param   Doku_Handler $handler The Doku_Handler object
      * @return  bool|array Return an array with all data you want to use in render, false don't add an instruction
      */
-    public function handle($match, $state, $pos, Doku_Handler $handler){
+    public function handle($match, $state, $pos, Doku_Handler $handler) {
         // Export button
-        if ($match == '~~ODT~~') {
+        if($match == '~~ODT~~') {
             return array();
         }
 
         // Extended info
-        $match = substr($match,6,-2); //strip markup
-        $extinfo = explode(':',$match);
+        $match = substr($match, 6, -2); //strip markup
+        $extinfo = explode(':', $match);
 
         $info_type = $extinfo[0];
 
-        if (count($extinfo) < 2) { // no value
+        if(count($extinfo) < 2) { // no value
             $info_value = '';
-        } elseif (count($extinfo) == 2) {
+        } elseif(count($extinfo) == 2) {
             $info_value = $extinfo[1];
         } else { // value may contain colons
-            $info_value = implode(array_slice($extinfo,1), ':');
+            $info_value = implode(array_slice($extinfo, 1), ':');
         }
         return array($info_type, $info_value);
     }
@@ -79,21 +79,23 @@ class syntax_plugin_odt extends DokuWiki_Syntax_Plugin {
     /**
      * Handles the actual output creation.
      *
-     * @param string          $format   output format being rendered
-     * @param Doku_Renderer   $renderer the current renderer object
-     * @param array           $data     data created by handler()
+     * @param string $format output format being rendered
+     * @param Doku_Renderer $renderer the current renderer object
+     * @param array $data data created by handler()
      * @return  boolean                 rendered correctly? (however, returned value is not used at the moment)
      */
     public function render($format, Doku_Renderer $renderer, $data) {
         global $ID, $REV;
 
-        if (!$data) { // Export button
+        if(!$data) { // Export button
             if($format != 'xhtml') return false;
-            $renderer->doc .= '<a href="'.exportlink($ID, 'odt', ($REV != '' ? 'rev='.$REV : '')).'" title="'.$this->getLang('view').'">';
-            $renderer->doc .= '<img src="'.DOKU_BASE.'lib/plugins/odt/odt.png" align="right" alt="'.$this->getLang('view').'" width="48" height="48" />';
+            $renderer->doc .= '<a href="' . exportlink($ID, 'odt', ($REV != '' ? 'rev=' . $REV : '')) . '" title="' . $this->getLang('view') . '">';
+            $renderer->doc .= '<img src="' . DOKU_BASE . 'lib/plugins/odt/odt.png" align="right" alt="' . $this->getLang('view') . '" width="48" height="48" />';
             $renderer->doc .= '</a>';
             return true;
+
         } else { // Extended info
+
             list($info_type, $info_value) = $data;
             if ($info_type == "template") { // Template-based export
                 $renderer->template = $info_value;

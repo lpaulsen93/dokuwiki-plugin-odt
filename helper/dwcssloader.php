18cedcc1c6ed6ac5c40e5210c00fd6fd10769f29
Gerrit Uitslag <klapinklapin@gmail.com>
2015-06-04 00:17:03 +0200

    dwcssloader: old code,phpdocs, close tag,duplicate keys array

--- helper/dwcssloader.php
+++ helper/dwcssloader.php
@@ -2,7 +2,7 @@
 /**
  * Helper class to load standrad DokuWiki CSS files.
  * Adopted code from dw2pdf plugin by Andreas Gohr <andi@splitbrain.org>.
- * 
+ *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @author     LarsDW223
  */
@@ -10,19 +10,22 @@
 // must be run within Dokuwiki
 if (!defined('DOKU_INC')) die();
 
-if (!defined('DOKU_LF')) define('DOKU_LF', "\n");
-if (!defined('DOKU_TAB')) define('DOKU_TAB', "\t");
-if (!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN', DOKU_INC.'lib/plugins/');
 
+/**
+ * Class helper_plugin_odt_dwcssloader
+ */
 class helper_plugin_odt_dwcssloader extends DokuWiki_Plugin {
+    /**
+     * @return array
+     */
     function getMethods() {
         $result = array();
         $result[] = array(
                 'name'   => 'load',
                 'desc'   => 'Loads standard DokuWiki, plugin specific and format specific CSS files and templates. Includes handling of replacements and less parsing.',
-                'params' => array('$plugin_name' => 'string'),
-                'params' => array('$format' => 'string'),
-                'params' => array('$template' => 'string'),
+                'params' => array('$plugin_name' => 'string',
+                                  '$format' => 'string',
+                                  '$template' => 'string'),
                 'return' => array('All CSS styles' => 'string'),
                 );
         return $result;
@@ -30,6 +33,10 @@ class helper_plugin_odt_dwcssloader extends DokuWiki_Plugin {
 
     /**
      * Load all the style sheets and apply the needed replacements
+     * @param $plugin_name
+     * @param $format
+     * @param $template
+     * @return string
      */
     public function load($plugin_name, $format, $template) {
         global $conf;
@@ -87,6 +94,9 @@ class helper_plugin_odt_dwcssloader extends DokuWiki_Plugin {
      * Checks for a $format.'.css', falls back to print.css
      *
      * @author Andreas Gohr <andi@splitbrain.org>
+     *
+     * @param string $format
+     * @return array
      */
     protected function css_pluginFormatStyles($format) {
         $list = array();
@@ -108,4 +118,3 @@ class helper_plugin_odt_dwcssloader extends DokuWiki_Plugin {
         return $list;
     }
 }
-?>

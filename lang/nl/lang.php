45c86aed79ae9c6f53bcf7d81a6f15d6a2a3720c
Peter van Diest <peter.van.diest@xs4all.nl>
2015-07-09 09:50:05 +0200

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -5,9 +5,11 @@
  * 
  * @author Wouter Wijsman <wwijsman@live.nl>
  * @author Arthur Buijs <arthur@artietee.nl>
+ * @author Peter van Diest <peter.van.diest@xs4all.nl>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
 $lang['view']                  = 'Exporteer pagina naar Open Document formaat';
 $lang['export_odt_button']     = 'ODT exporteren';
 $lang['tpl_not_found']         = 'WAARSCHUWING : het ODT-sjabloon "%s" is niet aanwezig in de directory voor sjablonen "%s". Het standaardsjabloon zal gebruikt worden.  ';
+$lang['toc_title']             = 'Inhoudsopgave';
--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -5,10 +5,10 @@
  * 
  * @author Wouter Wijsman <wwijsman@live.nl>
  * @author Arthur Buijs <arthur@artietee.nl>
+ * @author Peter van Diest <peter.van.diest@xs4all.nl>
  */
 $lang['tpl_dir']               = 'Subdirectory voor sjablonen in de mediabeheerder';
 $lang['showexportbutton']      = 'Toon de ODT exportknop';
-
+$lang['media_sel']             = 'Welke @mediaselector moet worden gebruikt om CSS-instellingen te zoeken?';
 $lang['template']              = 'Welke template moet worden gebruikt bij het creëren van ODT\'s?';
-$lang['usestyles']             = 'Je kunt een komma gescheiden lijst van plugins opgeven waarvan de  <code>style.css</code> of <code>screen.css</code> moeten worden gebruikt bij het genereren van de ODT. Standaard worden alleen <code>print.css</code> en <code>odt.css</code> gebruikt.';
-
+$lang['usestyles']             = 'Je kunt een komma-gescheiden lijst van plugins opgeven waarvan de  <code>style.css</code> of <code>screen.css</code> moeten worden gebruikt bij het genereren van de ODT. Standaard worden alleen <code>print.css</code> en <code>odt.css</code> gebruikt.';

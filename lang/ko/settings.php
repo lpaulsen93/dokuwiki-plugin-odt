455992e72f9c8c6318e2cb1064979c8b67c83264
Myeongjin <aranet100@gmail.com>
2015-07-11 10:05:06 +0200

    translation update

--- lang/ko/settings.php
+++ lang/ko/settings.php
@@ -4,10 +4,10 @@
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * 
  * @author HyunWoo Jo <showaid@gmail.com>
+ * @author Myeongjin <aranet100@gmail.com>
  */
-$lang['tpl_dir']               = '미디어 매니저 내 템플릿용 하위 디렉터리 ';
-$lang['showexportbutton']      = 'ODT 내보내기 버튼 표시';
-
-$lang['template']              = '어떤 템플릿을 ODT 파일의 형식에 사용되어야 합니까?';
+$lang['tpl_dir']               = '미디어 관리자에서 템플릿을 위한 하위 디렉터리';
+$lang['showexportbutton']      = 'ODT 내보내기 버튼 보이기';
+$lang['media_sel']             = '어떤 @미디어 선택기를 CSS 속성을 쿼리하는 데 사용합니까?';
+$lang['template']              = '어떤 템플릿을 ODT 파일의 형식에 사용해야 합니까?';
 $lang['usestyles']             = 'ODT 생성에 사용되어야 하는 어느 <code>style.css</code>나 <code>screen.css</code>의 플러그인의 쉼표로 구분된 목록을 제공할 수 있습니다. 기본적으로 <code>print.css</code>와 <code>odt.css</code>만 사용됩니다.';
-

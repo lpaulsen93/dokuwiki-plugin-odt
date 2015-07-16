3ff516339517f218d234f59ad4e457ac4c9dcd68
Gerrit Uitslag <klapinklapin@gmail.com>
2015-07-16 23:51:19 +0200

    Add missing language strings
    copied from dw2pdf

--- lang/eo/lang.php
+++ lang/eo/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Robert Bogenschneider <robog@gmx.de>
  */
 $lang['encoding']              = 'utf-8';
@@ -10,3 +10,6 @@ $lang['direction']             = 'ltr';
 $lang['view']                  = 'Eksporti paĝon al Open Document-formato';
 $lang['export_odt_button']     = 'ODT-eksporto';
 $lang['tpl_not_found']         = 'AVERTO: la ODT-ŝablono "%s" ne troviĝis en la ŝablona dosierujo "%s". Uzante la standardan ŝablonon.';
+$lang['needtitle']             = 'Bonvolu indiki titolon.';
+$lang['needns']                = 'Bv. indiki ekzistantan nomspacon.';
+$lang['empty']                 = 'Vi ankoraŭ ne selektis paĝojn.';
--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author NicolasFriedli <nicolas@theologique.ch>
  */
 $lang['encoding']              = 'utf-8';
@@ -10,3 +10,6 @@ $lang['direction']             = 'gauche à droite';
 $lang['view']                  = 'Exporter la page en format Open Document';
 $lang['export_odt_button']     = 'exportation ODT';
 $lang['tpl_not_found']         = 'ATTENTION: le modèle ODT "%s" est introuvable dans le répertoire "%s". Le modèle par défaut est utilisé.';
+$lang['needtitle']             = 'Veuillez choisir un titre.';
+$lang['needns']                = 'Veuillez choisir une catégorie existante.';
+$lang['empty']                 = 'Vous n\'avez pas encore sélectionné de pages.';

387719d8d7745082db477d14fc0992bc36d42732
Schplurtz le Déboulonné <schplurtz@laposte.net>
2015-07-20 19:30:36 +0200

    translation update

--- lang/fr/lang.php
+++ lang/fr/lang.php
@@ -2,14 +2,16 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  * @author NicolasFriedli <nicolas@theologique.ch>
+ * @author Schplurtz le Déboulonné <schplurtz@laposte.net>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'gauche à droite';
 $lang['view']                  = 'Exporter la page en format Open Document';
 $lang['export_odt_button']     = 'exportation ODT';
 $lang['tpl_not_found']         = 'ATTENTION: le modèle ODT "%s" est introuvable dans le répertoire "%s". Le modèle par défaut est utilisé.';
+$lang['toc_title']             = 'Table des matières';
 $lang['needtitle']             = 'Veuillez choisir un titre.';
 $lang['needns']                = 'Veuillez choisir une catégorie existante.';
 $lang['empty']                 = 'Vous n\'avez pas encore sélectionné de pages.';

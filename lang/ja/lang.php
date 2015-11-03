9007cae744f30b22ac36478e6f9444ab6ea948b3
Hideaki SAWADA <chuno@live.jp>
2015-11-03 19:25:33 +0100

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -2,7 +2,7 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  * @author Hideaki SAWADA <chuno@live.jp>
  */
 $lang['encoding']              = 'utf-8';
@@ -11,6 +11,6 @@ $lang['view']                  = 'OpenDocument 形式でページを出力する
 $lang['export_odt_button']     = 'ODT 出力';
 $lang['tpl_not_found']         = '警告：ODTテンプレート "%s" はテンプレートディレクトリ "%s" にありませんでした。デフォルトのテンプレートを使用します。';
 $lang['toc_title']             = '目次';
-$lang['needtitle']         = "題名を入力して下さい。";
-$lang['needns']            = "名前空間を入力して下さい。";
-$lang['empty']             = "ページを選択していません。";
+$lang['needtitle']             = '題名を入力して下さい。';
+$lang['needns']                = '名前空間を入力して下さい。';
+$lang['empty']                 = 'ページを選択していません。';

8966b37aa83d718e1d6d9427efe4e40e59e1b32f
LarsDW223 <lars_paulsen@web.de>
2016-01-16 15:28:54 +0100

    Added regression test code for issue #123. See #123.

--- _test/odt.styles.paragraphstyle.test.php
+++ _test/odt.styles.paragraphstyle.test.php
@@ -212,4 +212,82 @@ class plugin_odt_paragraphstyle_test extends DokuWikiTest {
         $this->assertEquals('Bitstream Vera Sans2', $attributes['style:font-name-asian']);
         $this->assertEquals('Bitstream Vera Sans2', $attributes['style:font-name-complex']);
     }
+
+    /**
+     * Test setProperty() and toString(), including tab-stops.
+     * This is a test case for issue #123.
+     */
+    public function test_set_toc_paragraph() {
+        $indent = 2;
+        $properties = array();
+        $properties ['style-name']         = 'TOC-Test';
+        $properties ['style-parent']       = 'Index';
+        $properties ['style-class']        = 'index';
+        $properties ['style-position']     = 17 - $indent .'cm';
+        $properties ['style-type']         = 'right';
+        $properties ['style-leader-style'] = 'dotted';
+        $properties ['style-leader-text']  = '.';
+        $properties ['margin-left']        = $indent.'cm';
+        $properties ['margin-right']       = '0cm';
+        $properties ['text-indent']        = '0cm';
+
+        // This variable is just used to show the expected result but is not used for test comparsion.
+        // We explicitly parse the exported XML code to be independent from attributes position.
+        // The attrbitues positions might change in the future if ODTParagraphStyle.php is changed.
+        $expected  = '<style:style style:name="TOC-Test" style:parent-style-name="Index" style:class="index" style:family="paragraph" >';
+        $expected .= '<style:paragraph-properties fo:margin-left="2cm" fo:margin-right="0cm" fo:text-indent="0cm" >';
+        $expected .= '<style:tab-stops>';
+        $expected .= '<style:tab-stop style:position="15cm" style:type="right" style:leader-style="dotted" style:leader-text="." />';
+        $expected .= '</style:tab-stops>';
+        $expected .= '</style:paragraph-properties>';
+        $expected .= '</style:style>';
+
+        // Create style, set all properties and get XML code of the style
+        $style = new ODTParagraphStyle();
+        $this->assertNotNull($style);
+
+        foreach ($properties as $key => $value) {
+            $style->setProperty($key, $value);
+        }
+        $style_string = $style->toString();
+
+        // We should have the following elements:
+        // style:style, style:paragraph-properties, style:text-properties, style:tab-stops
+        $style_style = XMLUtil::getElementOpenTag('style:style', $style_string);
+        $this->assertNotNull($style_style);
+        $paragraph_props = XMLUtil::getElementOpenTag('style:paragraph-properties', $style_string);
+        $paragraph = XMLUtil::getElement('style:paragraph-properties', $style_string);
+        $this->assertNotNull($paragraph_props);
+        $tab_stops_props = XMLUtil::getElement('style:tab-stops', $paragraph);
+        $this->assertNotNull($tab_stops_props);
+        $tab_stop_props = XMLUtil::getElementOpenTag('style:tab-stop', $tab_stops_props);
+        $this->assertNotNull($tab_stop_props);
+
+        // Check attribute values of element "style:style", see $expected
+        // Remark: attribute 'style:family' must always be present even if it was not set
+        $attributes = array();
+        $found = XMLUtil::getAttributes($attributes, $style_style);
+        $this->assertEquals(4, $found);
+        $this->assertEquals('TOC-Test', $attributes['style:name']);
+        $this->assertEquals('Index', $attributes['style:parent-style-name']);
+        $this->assertEquals('index', $attributes['style:class']);
+        $this->assertEquals('paragraph', $attributes['style:family']);
+
+        // Check attribute values of element "style:paragraph-properties", see $expected
+        $attributes = array();
+        $found = XMLUtil::getAttributes($attributes, $paragraph_props);
+        $this->assertEquals(3, $found);
+        $this->assertEquals('2cm', $attributes['fo:margin-left']);
+        $this->assertEquals('0cm', $attributes['fo:margin-right']);
+        $this->assertEquals('0cm', $attributes['fo:text-indent']);
+
+        // Check attribute values of element "style:tab-stop", see $expected
+        $attributes = array();
+        $found = XMLUtil::getAttributes($attributes, $tab_stop_props);
+        $this->assertEquals(4, $found);
+        $this->assertEquals('15cm', $attributes['style:position']);
+        $this->assertEquals('right', $attributes['style:type']);
+        $this->assertEquals('dotted', $attributes['style:leader-style']);
+        $this->assertEquals('.', $attributes['style:leader-text']);
+    }
 }

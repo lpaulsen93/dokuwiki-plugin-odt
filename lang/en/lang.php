68cad3c5ecb6c46eb435aabd338d6958e7c1e8bd
Gerrit Uitslag <klapinklapin@gmail.com>
2016-08-10 23:47:07 +0200

    Improve message

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -22,7 +22,7 @@ $lang['update_chapter_msg'] = 'Please remember to update the Chapter Index after
 $lang['needtitle']         = "Please provide a title.";
 $lang['needns']            = "Please provide an existing namespace.";
 $lang['empty']             = "You don't have pages selected yet.";
-$lang['forbidden']         = "You have no access to these pages: %s. Use option 'Skip Forbidden Pages' or add '&book_skipforbiddenpages=1' to your export-url";
+$lang['forbidden']         = "You have no access to these pages: %s.<br/><br/>Use option 'Skip Forbidden Pages' to create your book with the available pages.";
 
 // Error message for failed conversion.
 // The following replacments are supported:

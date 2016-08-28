8394b7859a8931dc86c08dfd9c44f7faef6393bf
LarsGit223 <LarsGit223@users.noreply.github.com>
2016-08-28 14:14:08 +0200

    Revert "Merge latest changes from master branch"

--- lang/nl/settings.php
+++ lang/nl/settings.php
@@ -2,25 +2,13 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
+ * 
  * @author Wouter Wijsman <wwijsman@live.nl>
  * @author Arthur Buijs <arthur@artietee.nl>
  * @author Peter van Diest <peter.van.diest@xs4all.nl>
- * @author Marcel Bachus <marcel.bachus@ziggo.nl>
  */
 $lang['tpl_dir']               = 'Subdirectory voor sjablonen in de mediabeheerder';
 $lang['showexportbutton']      = 'Toon de ODT exportknop';
 $lang['media_sel']             = 'Welke @mediaselector moet worden gebruikt om CSS-instellingen te zoeken?';
 $lang['css_template']          = 'Welke template moet worden gebruikt bij het creëren van ODT\'s?';
 $lang['usestyles']             = 'Je kunt een komma-gescheiden lijst van plugins opgeven waarvan de  <code>style.css</code> of <code>screen.css</code> moeten worden gebruikt bij het genereren van de ODT. Standaard worden alleen <code>print.css</code> en <code>odt.css</code> gebruikt.';
-$lang['format']                = 'Pagina formaat';
-$lang['orientation']           = 'Pagina Orientatie';
-$lang['margin_top']            = 'Pagina top marge in cm';
-$lang['margin_right']          = 'Pagina rechter marge in cm';
-$lang['margin_bottom']         = 'Pagina beneden marge in cm';
-$lang['margin_left']           = 'Pagina linker marge in cm';
-$lang['disable_links']         = 'Aanmake van Link uitsluiten?';
-$lang['toc_maxlevel']          = 'Maximum aantal niveau\'s voor Inhoudsopgave';
-$lang['toc_indents']           = 'Inhoudsopgave: Inspringen per niveau in cm';
-$lang['toc_pagebreak']         = 'Plaats pagina-einde na Inhoudsopgave?';
-$lang['toc_style']             = 'Tekst Stijl voor Inhoudsopgave';

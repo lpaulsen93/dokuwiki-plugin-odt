2f543fea0e01a1bdea7467530e792648ae1534e0
Ing. Jan Kaláb <jan+dokuwiki@kalabovi.org>
2016-09-02 14:15:06 +0200

    translation update

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -2,15 +2,21 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
+ * @author Ing. Jan Kaláb <jan+dokuwiki@kalabovi.org>
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
 $lang['view']                  = 'Exportovat stránku ve formátu Open Document';
 $lang['export_odt_button']     = 'ODT export';
-$lang['tpl_not_found']         = 'VAROVÁNÍ : ODT šablona "%s" nebyla nalezena v adresáři se šablonami "%s". Bude použita výchozí šablona.';
+$lang['tpl_not_found']         = 'CHYBA : ODT šablona "%s" nebyla nalezena v adresáři se šablonami "%s". Bude použita výchozí šablona.';
 $lang['toc_title']             = 'Obsah';
+$lang['chapter_title']         = 'Index kapitol';
+$lang['toc_msg']               = 'Sem bude vložen obsah.';
+$lang['chapter_msg']           = 'Sem bude vložen index kapitol.';
+$lang['update_toc_msg']        = 'Prosím nezapomeňte aktualizovat obsah po exportu.';
+$lang['update_chapter_msg']    = 'Prosím nezapomeňte aktualizovat index kapitol po exportu.';
 $lang['needtitle']             = 'Vlože prosím nadpis.';
 $lang['needns']                = 'Vložte prosím existující jmenný prostor.';
 $lang['empty']                 = 'Zatím jste nevybrali žádné stránky.';
--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -2,11 +2,24 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
+ * @author Ing. Jan Kaláb <jan+dokuwiki@kalabovi.org>
  */
 $lang['tpl_dir']               = 'Podadresář se šablonami ve správci médií';
 $lang['showexportbutton']      = 'Zobrazovat tlačítko pro export do ODT ';
 $lang['media_sel']             = 'Jaký @media selektor má být použit pro zjišťování CSS vlastností?';
 $lang['css_template']          = 'Jaká šablona má být použita pro formátování ODT souborů?';
 $lang['usestyles']             = 'Můžete zadat čárkou dělený seznam zásuvných modulů, jejichž <code>style.css</code> nebo <code>screen.css</code> má být použit pro vytváření ODT. Ve výchozím nastavení jsou použity pouze <code>print.css</code> a <code>odt.css</code>.';
+$lang['format']                = 'formát stránky';
+$lang['orientation']           = 'Aorientace stránky';
+$lang['margin_top']            = 'Horní okraj v cm';
+$lang['margin_right']          = 'Pravý okraj v cm';
+$lang['margin_bottom']         = 'Spodní okraj v cm';
+$lang['margin_left']           = 'Levý okraj v cm';
+$lang['disable_links']         = 'Zakázat vytváření odkazů?';
+$lang['toc_maxlevel']          = 'Maximální hloubka obsahu';
+$lang['toc_leader_sign']       = 'Nadpis pro obsah';
+$lang['toc_indents']           = 'Obsah: odsazení úrovní v cm';
+$lang['toc_pagebreak']         = 'Zalomení stránky za obsahem?';
+$lang['toc_style']             = 'Styl textu obsahu';

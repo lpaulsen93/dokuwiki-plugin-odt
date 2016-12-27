5623c7b4887abc73498f6269d97f865eba213ab7
Hideaki SAWADA <chuno@live.jp>
2016-12-27 01:10:28 +0100

    translation update

--- lang/ja/lang.php
+++ lang/ja/lang.php
@@ -2,10 +2,8 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Hideaki SAWADA <chuno@live.jp>
- * 
- * @package DokuWiki\lang\ja\lang
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
@@ -13,6 +11,11 @@ $lang['view']                  = 'OpenDocument 形式でページを出力する
 $lang['export_odt_button']     = 'ODT 出力';
 $lang['tpl_not_found']         = '警告：ODTテンプレート "%s" はテンプレートディレクトリ "%s" にありませんでした。デフォルトのテンプレートを使用します。';
 $lang['toc_title']             = '目次';
+$lang['chapter_title']         = '章索引';
+$lang['toc_msg']               = '目次をここに挿入します。';
+$lang['chapter_msg']           = '章索引をここに挿入します。';
+$lang['update_toc_msg']        = '出力後に目次を更新することを忘れないでください。';
+$lang['update_chapter_msg']    = '出力後に章索引を更新することを忘れないでください。';
 $lang['needtitle']             = '題名を入力して下さい。';
 $lang['needns']                = '名前空間を入力して下さい。';
 $lang['empty']                 = 'ページを選択していません。';
--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -2,15 +2,14 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
+ *
  * @author Hideaki SAWADA <chuno@live.jp>
- * 
- * @package DokuWiki\lang\ja\settings
  */
 $lang['tpl_dir']               = 'メディアマネージャー内のテンプレート用サブディレクトリ';
 $lang['showexportbutton']      = 'ODT 出力ボタンを表示';
 $lang['media_sel']             = 'CSS プロパティ参照時の @media セレクタ';
 $lang['css_template']          = 'ODT の整形に使用するテンプレート';
+$lang['usestyles']             = 'ODT 作成で使用する <code>style.css</code> や <code>screen.css</code> のカンマ区切り一覧。デフォルトでは <code>print.css</code> と <code>odt.css</code> だけを使用します。';
 $lang['format']                = 'ページ形式';
 $lang['orientation']           = 'ページの向き';
 $lang['margin_top']            = '上ページ余白（cm）';
@@ -19,6 +18,7 @@ $lang['margin_bottom']         = '下ページ余白（cm）';
 $lang['margin_left']           = '左ページ余白（cm）';
 $lang['disable_links']         = 'リンク作成の無効化';
 $lang['toc_maxlevel']          = '目次の深さ';
+$lang['toc_leader_sign']       = '目次の印';
 $lang['toc_indents']           = '目次：レベル毎の字下げ（cm）';
 $lang['toc_pagebreak']         = '目次直後の改ページ';
 $lang['toc_style']             = '目次の文字形式';

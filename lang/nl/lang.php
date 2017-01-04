1fe98221b01331fb16cd9c5d7b196c9758604ef7
Rudy V <rvantrappen@gmail.com>
2017-01-04 16:50:16 +0100

    translation update

--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -6,9 +6,6 @@
  * @author Wouter Wijsman <wwijsman@live.nl>
  * @author Arthur Buijs <arthur@artietee.nl>
  * @author Peter van Diest <peter.van.diest@xs4all.nl>
- * @author Marcel Bachus <marcel.bachus@ziggo.nl>
- * 
- * @package DokuWiki\lang\nl\lang
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
@@ -16,11 +13,6 @@ $lang['view']                  = 'Exporteer pagina naar Open Document formaat';
 $lang['export_odt_button']     = 'ODT exporteren';
 $lang['tpl_not_found']         = 'WAARSCHUWING : het ODT-sjabloon "%s" is niet aanwezig in de directory voor sjablonen "%s". Het standaardsjabloon zal gebruikt worden.  ';
 $lang['toc_title']             = 'Inhoudsopgave';
-$lang['chapter_title']         = 'Hoofdstuk Index';
-$lang['toc_msg']               = 'Inhoudsopgave zal hier geplaatst worden.';
-$lang['chapter_msg']           = 'Hoofdstuk opgave zal hier geplaatst worden.';
-$lang['update_toc_msg']        = 'Denk eraan om de Inhoudsopgave bij te werken na uitvoer.';
-$lang['update_chapter_msg']    = 'Denk eraan om de Hoofdstuk opgave bij te werken na uitvoer.';
 $lang['needtitle']             = 'Er moet een titel ingevuld worden';
 $lang['needns']                = 'Geef een bestaande namespace.';
 $lang['empty']                 = 'Er zijn nog geen pagina\'s geselecteerd.';

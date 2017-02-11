d65bf4cea20af90252a60e66c249c21f6cc3716e
LarsDW223 <lars_paulsen@web.de>
2017-02-11 11:24:52 +0100

    Added adjusted translations from pull-request #171. Closes #171.

--- lang/ja/settings.php
+++ lang/ja/settings.php
@@ -19,6 +19,7 @@ $lang['margin_bottom']         = '下ページ余白（cm）';
 $lang['margin_left']           = '左ページ余白（cm）';
 $lang['disable_links']         = 'リンク作成の無効化';
 $lang['toc_maxlevel']          = '目次の深さ';
+$lang['toc_leader_sign']       = '目次の印';
 $lang['toc_indents']           = '目次：レベル毎の字下げ（cm）';
 $lang['toc_pagebreak']         = '目次直後の改ページ';
 $lang['toc_style']             = '目次の文字形式';

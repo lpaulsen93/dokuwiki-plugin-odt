6e4c9bea1ff4bced97ed641587da3981ebaa5228
Vojta Olsan <olsh0@seznam.cz>
2017-06-16 18:10:33 +0200

    translation update

--- lang/cs/settings.php
+++ lang/cs/settings.php
@@ -5,15 +5,17 @@
  *
  * @author Jaroslav Lichtblau <jlichtblau@seznam.cz>
  * @author Ing. Jan Kaláb <jan+dokuwiki@kalabovi.org>
- * 
- * @package DokuWiki\lang\cs\settings
+ * @author Vojta Olsan <olsh0@seznam.cz>
  */
 $lang['tpl_dir']               = 'Podadresář se šablonami ve správci médií';
+$lang['odt_template']          = 'Šablona pro import stylů (soubor ODT nebo CSS)';
 $lang['showexportbutton']      = 'Zobrazovat tlačítko pro export do ODT ';
+$lang['css_usage']             = 'Aplikovat importované CSS do ODT stylů?';
 $lang['media_sel']             = 'Jaký @media selektor má být použit pro zjišťování CSS vlastností?';
 $lang['css_template']          = 'Jaká šablona má být použita pro formátování ODT souborů?';
-$lang['format']                = 'formát stránky';
-$lang['orientation']           = 'Aorientace stránky';
+$lang['apply_fs_to_non_css']   = 'Aplikovat velikost písma CSS na ODT šablonu a plugin implicitních stylů?';
+$lang['format']                = 'Formát stránky';
+$lang['orientation']           = 'Orientace stránky';
 $lang['margin_top']            = 'Horní okraj v cm';
 $lang['margin_right']          = 'Pravý okraj v cm';
 $lang['margin_bottom']         = 'Spodní okraj v cm';
@@ -24,3 +26,4 @@ $lang['toc_leader_sign']       = 'Nadpis pro obsah';
 $lang['toc_indents']           = 'Obsah: odsazení úrovní v cm';
 $lang['toc_pagebreak']         = 'Zalomení stránky za obsahem?';
 $lang['toc_style']             = 'Styl textu obsahu';
+$lang['convert_to_pdf']        = 'Nařídit spuštění konverze do PDF';

56c0a3e1570e23493d9286756d4ecc15524d0661
2576562185 <2576562185@qq.com>
2017-08-27 11:35:27 +0200

    translation update

---
+++ lang/zh/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 2576562185 <2576562185@qq.com>
+ */
+$lang['view']                  = '将页面以Open Document格式导出';
+$lang['export_odt_button']     = '以ODT格式导出';
+$lang['needtitle']             = '请填写标题。';
+$lang['needns']                = '请提供一个已经存在的命名空间。';
+$lang['empty']                 = '您还没有选择页面。';
---
+++ lang/zh/settings.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author 2576562185 <2576562185@qq.com>
+ */
+$lang['format']                = '页面格式';
+$lang['orientation']           = '页面方向';

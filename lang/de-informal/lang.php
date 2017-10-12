5c2325bb23a192d187843c981d0c8798de21c581
F. Mueller-Donath <j.felix@mueller-donath.de>
2017-10-12 11:25:22 +0200

    translation update

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -3,9 +3,8 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  * @author Florian Lamml <info@florian-lamml.de>
- *
- * @package DokuWiki\lang\de-informal\lang
  */
 $lang['encoding']              = 'utf-8';
 $lang['direction']             = 'ltr';
@@ -17,19 +16,13 @@ $lang['toc_title']             = 'Inhaltsverzeichnis';
 $lang['chapter_title']         = 'Kapitelinhalt';
 $lang['toc_msg']               = 'Ein Inhaltsverzeichnis wird an dieser Stelle eingefügt.';
 $lang['chapter_msg']           = 'Ein Kapitelverzeichnis wird an dieser Stelle eingefügt.';
-$lang['update_toc_msg']        = 'Bitte vergessen Sie nicht, das Inhaltsverzeichnis nach dem Export zu aktualisieren.';
-$lang['update_chapter_msg']    = 'Bitte vergessen Sie nicht, das Kapitelverzeichnis nach dem Export zu aktualisieren.';
+$lang['update_toc_msg']        = 'Bitte vergiss nicht, das Inhaltsverzeichnis nach dem Export zu aktualisieren.';
+$lang['update_chapter_msg']    = 'Bitte vergiss nicht, das Kapitelverzeichnis nach dem Export zu aktualisieren.';
 $lang['needtitle']             = 'Bitte Titel angeben!';
-$lang['needns']                = 'Bitte geben Sie einen vorhandenen Namensraum an.';
-$lang['empty']                 = 'Sie haben noch keine Seiten gewählt.';
-
-// Error message for failed conversion.
-// The following replacments are supported:
-// %command%      = the complete command line which was executed
-// %errorcode%    = the error code reported after executing the command
-// %errormessage% = the detailed error message reported after executing the command
-$lang['conversion_failed_msg'] =
-'====== Bei der Konvertierung des ODT-Dokuments ist ein Fehler aufgetreten: ======
+$lang['needns']                = 'Bitte gib einen vorhandenen Namensraum an.';
+$lang['empty']                 = 'Du hast noch keine Seiten ausgewählt.';
+$lang['forbidden']             = 'Du hast keine Berechtigungen für folgende Seiten: %s.<br/><br/>Benutze die Option "Verbotene Seiten überspringen" um ein Buch nur mit den verfügbaren Seiten zu erstellen.';
+$lang['conversion_failed_msg'] = '====== Bei der Konvertierung des ODT-Dokuments ist ein Fehler aufgetreten: ======
 
 Ausgeführte Kommando-Zeile:
 
@@ -42,14 +35,11 @@ Fehlerausgabe:
 <code>%errormessage%</code>
 
 [[%pageid%|Zurück zur vorherigen Seite]]';
+$lang['init_failed_msg']       = '====== Ein Fehler ist bei der Initialisierung des ODT-Dokuments aufgetreten: ======
 
-// Error message for failed conversion.
-$lang['init_failed_msg'] =
-'====== Ein Fehler ist bei der Initialisierung des ODT-Dokuments aufgetreten: ======
-
-Ist Ihre DokuWiki-Version kompatibel zum ODT-Plugin?
+Ist deine DokuWiki-Version kompatibel zum ODT-Plugin?
 
 Seit Release 2017-02-11 benötigt das ODT-Plugin DokuWiki-Release “Detritus” oder neuer!
-Für detailierte Informationen lesen Sie bitte den [[https://www.dokuwiki.org/plugin:odt#requirements|Abschnitt "Requirements"]] auf der ODT-Plugin Seite auf DokuWiki.org.
+Für detailierte Informationen lies bitte den [[https://www.dokuwiki.org/plugin:odt#requirements|Abschnitt "Requirements"]] auf der ODT-Plugin Seite auf DokuWiki.org.
 
-(Ihr DokuWiki-Release ist %DWVERSION%)';
+(Dein DokuWiki-Release ist %DWVERSION%)';

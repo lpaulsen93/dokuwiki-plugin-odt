3a2efe837053b86ba5f3647a785d1932025fc254
Yuriy Skalko <yuriy.skalko@gmail.com>
2018-03-29 23:00:41 +0200

    translation update

--- lang/ru/settings.php
+++ lang/ru/settings.php
@@ -3,23 +3,29 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author Yuriy Skalko <yuriy.skalko@gmail.com>
  * @author Anotheroneuser <w20151222@ya.ru>
  * @author Wirbel78 <bryanskmap@yandex.ru>
  */
-$lang['tpl_dir']               = 'Подкатегория для шаблонов в организаторе аудиовизуальной информации. ';
-$lang['odt_template']          = 'Шаблон стиля импорта (файл ODT CSS)';
-$lang['showexportbutton']      = 'Высветить кнопку преобразования в формат ODT';
-$lang['media_sel']             = 'Какой @media переключатель должен быть использован для запроса свойств CSS? ';
-$lang['css_template']          = 'Какой шаблон должен быть использован для форматирования материалов ODT? ';
+$lang['tpl_dir']               = 'Подкаталог для шаблонов в управлении медиафайлами';
+$lang['odt_template']          = 'Шаблон стиля импорта (файл ODT или CSS)';
+$lang['showexportbutton']      = 'Показать кнопку преобразования в формат ODT';
+$lang['css_usage']             = 'Применить импортированный CSS к стилям ODT?';
+$lang['media_sel']             = 'Какой селектор @media должен быть использован для запроса свойств CSS? ';
+$lang['css_font_size']         = 'Базовый размер шрифта CSS (задающий 1em)';
+$lang['css_template']          = 'Какой шаблон должен быть использован для форматирования файлов ODT? ';
+$lang['apply_fs_to_non_css']   = 'Применить размер шрифта CSS для шаблона ODT и стилей плагина по умолчанию?';
 $lang['format']                = 'Формат страницы';
 $lang['orientation']           = 'Ориентация страницы';
-$lang['margin_top']            = 'Верхняя граница страницы в сантиметрах';
-$lang['margin_right']          = 'Правая граница страницы в сантиметрах';
-$lang['margin_bottom']         = 'Нижняя граница страницы в сантиметрах';
-$lang['margin_left']           = 'Левая граница страницы в сантиметрах';
+$lang['margin_top']            = 'Верхнее поле страницы (см)';
+$lang['margin_right']          = 'Правое поле страницы (см)';
+$lang['margin_bottom']         = 'Нижнее поле страницы (см)';
+$lang['margin_left']           = 'Левое поле страницы (см)';
 $lang['disable_links']         = 'Исключить создание ссылок? ';
-$lang['toc_maxlevel']          = 'Предельная глубина оглавления. ';
+$lang['toc_maxlevel']          = 'Предельная глубина оглавления';
 $lang['toc_leader_sign']       = 'Наименование оглавления';
-$lang['toc_indents']           = 'Оглавление: отступ в сантиметрах';
+$lang['toc_indents']           = 'Оглавление: отступ на уровень (см)';
 $lang['toc_pagebreak']         = 'Добавить разрыв страницы после оглавления?';
-$lang['toc_style']             = 'Стиль текста в оглавлении. ';
+$lang['toc_style']             = 'Стиль текста в оглавлении';
+$lang['index_in_browser']      = 'Показывать заполнитель для оглавления/индекса в браузере?';
+$lang['convert_to_pdf']        = 'Команда для запуска преобразования PDF';

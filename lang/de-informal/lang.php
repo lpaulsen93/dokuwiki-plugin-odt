d0e7251b2a26d820ad7a50cb4a5b168a6fdcb596
Buy soundcloud reposts <jodi.tran@gmx.de>
2018-06-29 12:35:12 +0200

    translation update

--- lang/de-informal/lang.php
+++ lang/de-informal/lang.php
@@ -5,43 +5,30 @@
  *
  * @package DokuWiki\lang\de-informal\lang
  *
+ * @author Buy soundcloud reposts <jodi.tran@gmx.de>
  * @author F. Mueller-Donath <j.felix@mueller-donath.de>
  * @author Florian Lamml <info@florian-lamml.de>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['view']                  = 'Die Seite im Open Document Format exportieren';
-$lang['export_odt_button']     = 'ODT exportieren';
-$lang['export_odt_pdf_button'] = 'ODT=>PDF exportieren';
-$lang['tpl_not_found']         = 'FEHLER: Die ODT Vorlage "%s" konnte im Vorlagenverzeichnis "%s" nicht gefunden werden. Der Export wurde abgebrochen.';
-$lang['toc_title']             = 'Inhaltsverzeichnis';
-$lang['chapter_title']         = 'Kapitelinhalt';
-$lang['toc_msg']               = 'Ein Inhaltsverzeichnis wird an dieser Stelle eingefügt.';
-$lang['chapter_msg']           = 'Ein Kapitelverzeichnis wird an dieser Stelle eingefügt.';
-$lang['update_toc_msg']        = 'Bitte vergiss nicht, das Inhaltsverzeichnis nach dem Export zu aktualisieren.';
-$lang['update_chapter_msg']    = 'Bitte vergiss nicht, das Kapitelverzeichnis nach dem Export zu aktualisieren.';
-$lang['needtitle']             = 'Bitte Titel angeben!';
-$lang['needns']                = 'Bitte gib einen vorhandenen Namensraum an.';
-$lang['empty']                 = 'Du hast noch keine Seiten ausgewählt.';
-$lang['forbidden']             = 'Du hast keine Berechtigungen für folgende Seiten: %s.<br/><br/>Benutze die Option "Verbotene Seiten überspringen" um ein Buch nur mit den verfügbaren Seiten zu erstellen.';
-$lang['conversion_failed_msg'] = '====== Bei der Konvertierung des ODT-Dokuments ist ein Fehler aufgetreten: ======
-
-Ausgeführte Kommando-Zeile:
-
-<code>%command%</code>
-
-Fehlerwert: %errorcode%
-
-Fehlerausgabe:
-
-<code>%errormessage%</code>
-
-[[%pageid%|Zurück zur vorherigen Seite]]';
-$lang['init_failed_msg']       = '====== Ein Fehler ist bei der Initialisierung des ODT-Dokuments aufgetreten: ======
-
-Ist deine DokuWiki-Version kompatibel zum ODT-Plugin?
-
-Seit Release 2017-02-11 benötigt das ODT-Plugin DokuWiki-Release “Detritus” oder neuer!
-Für detailierte Informationen lies bitte den [[https://www.dokuwiki.org/plugin:odt#requirements|Abschnitt "Requirements"]] auf der ODT-Plugin Seite auf DokuWiki.org.
-
-(Dein DokuWiki-Release ist %DWVERSION%)';
+$lang['encoding']              = 'It\'s going to be end of mine day, except before 
+finish I am reading this great post to increase my knowledge.';
+$lang['direction']             = 'It\'s going to be end of mine day, except before finish I am 
+reading this great post to increase my knowledge.';
+$lang['view']                  = 'Miami Springs';
+$lang['export_odt_button']     = 'Miami Springs';
+$lang['export_odt_pdf_button'] = 'Miami Springs';
+$lang['tpl_not_found']         = 'Miami Springs';
+$lang['toc_title']             = 'Just wanted to say Hello.';
+$lang['chapter_title']         = 'Just wanted to say Hello.';
+$lang['toc_msg']               = 'It\'s going to be end of mine day, except before finish I am reading 
+this great post to increase my knowledge.';
+$lang['chapter_msg']           = 'It\'s going to be end of mine day, except before finish 
+I am reading this great post to increase my knowledge.';
+$lang['update_toc_msg']        = 'Miami Springs';
+$lang['update_chapter_msg']    = 'Miami Springs';
+$lang['needtitle']             = 'Just wanted to say Hello.';
+$lang['needns']                = 'Tran';
+$lang['empty']                 = 'It\'s going to be end of mine day, except before finish 
+I am reading this great post to increase my knowledge.';
+$lang['forbidden']             = 'Comic';
+$lang['conversion_failed_msg'] = 'It\'s going to be end of mine day, except before finish I am reading this great post to increase my knowledge.';
+$lang['init_failed_msg']       = 'http://Electromusicnetwork.com/';

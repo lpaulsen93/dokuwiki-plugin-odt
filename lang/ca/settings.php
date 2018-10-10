5593691305b857dba5a56a0615f809edcc757323
Adolfo Jayme Barrientos <fito@libreoffice.org>
2018-10-10 16:55:34 +0200

    translation update

---
+++ lang/ca/settings.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Adolfo Jayme Barrientos <fito@libreoffice.org>
+ */
+$lang['tpl_dir']               = 'Subdirectori per a les plantilles al gestor multimèdia';
+$lang['odt_template']          = 'Plantilla per a la importació d’estils (fitxer ODT o CSS)';
+$lang['showexportbutton']      = 'Mostra el botó d’exportació a ODT';
+$lang['css_usage']             = 'Voleu aplicar el CSS importat als estils ODT?';
+$lang['media_sel']             = 'Quin selector @media s’ha d’utilitzar per a consultar els estils CSS?';
+$lang['css_font_size']         = 'Mida de lletra bàsica del CSS (defineix 1em)';
+$lang['css_template']          = 'Quina plantilla s’ha d’utilitzar per a formatar els fitxers ODT?';
+$lang['apply_fs_to_non_css']   = 'Voleu aplicar la mida de lletra del CSS a la plantilla ODT i als estils per defecte dels connectors?';
+$lang['twips_per_pixel_x']     = 'Twips per píxel (eix X)';
+$lang['twips_per_pixel_y']     = 'Twips per píxel (eix Y)';
+$lang['format']                = 'Format de la pàgina';
+$lang['orientation']           = 'Orientació de la pàgina';
+$lang['margin_top']            = 'Marge superior de la pàgina en cm';
+$lang['margin_right']          = 'Marge dret de la pàgina en cm';
+$lang['margin_bottom']         = 'Marge inferior de la pàgina en cm';
+$lang['margin_left']           = 'Marge esquerre de la pàgina en cm';
+$lang['disable_links']         = 'Voleu desactivar la creació d’enllaços?';
+$lang['toc_maxlevel']          = 'Profunditat màxima de la taula de continguts';
+$lang['toc_leader_sign']       = 'Signe guia per a la taula de continguts';
+$lang['toc_indents']           = 'Taula de continguts: sagnat per nivell en cm';
+$lang['toc_pagebreak']         = 'Voleu inserir un salt de pàgina després de la taula de continguts?';
+$lang['toc_style']             = 'Estil de text per a la taula de continguts';
+$lang['index_in_browser']      = 'Voleu mostrar al navegador un espai reservat per a la taula de continguts/l’índex?';
+$lang['outline_list_style']    = 'Estil de contorn';
+$lang['convert_to_pdf']        = 'Ordre que executar per a la conversió a PDF';

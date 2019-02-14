7969013fa4e5073e61f2601fc631f1e77c3b6c5a
RainSlide <RainSlide@outlook.com>
2019-02-14 17:40:51 +0100

    translation update

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -3,11 +3,12 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author RainSlide <RainSlide@outlook.com>
  * @author Wen Qixiang <wen@bootingman.org>
  * @author 2576562185 <2576562185@qq.com>
  */
 $lang['encoding']              = 'UTF-8';
-$lang['view']                  = '将页面以Open Document格式导出';
+$lang['view']                  = '将页面以 Open Document 格式导出';
 $lang['export_odt_button']     = '以ODT格式导出';
 $lang['export_odt_pdf_button'] = 'ODT=>PDF 导出';
 $lang['needtitle']             = '请填写标题。';
--- lang/zh/settings.php
+++ lang/zh/settings.php
@@ -3,7 +3,13 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author RainSlide <RainSlide@outlook.com>
  * @author 2576562185 <2576562185@qq.com>
  */
+$lang['showexportbutton']      = '显示导出 ODT 按钮';
 $lang['format']                = '页面格式';
 $lang['orientation']           = '页面方向';
+$lang['margin_top']            = '页面上边距（cm）';
+$lang['margin_right']          = '页面下边距（cm）';
+$lang['margin_bottom']         = '页面左边距（cm）';
+$lang['margin_left']           = '页面右边距（cm）';

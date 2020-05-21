a685565327353bb1ae482784927e8a32232242a2
Jacob Palm <jacobpalmdk@icloud.com>
2020-05-22 00:25:18 +0200

    translation update

--- lang/da/settings.php
+++ lang/da/settings.php
@@ -1,16 +1,25 @@
 <?php
+
 /**
  * Danish language file for config settings.
- * 
+ *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Søren Birk <soer9648@eucl.dk>
- * @author     Jacob Palm <mail@jacobpalm.dk>
  * @package    DokuWiki\lang\da\settings
+ * Translations 
+ *
+ * @author Jacob Palm <jacobpalmdk@icloud.com>
+ * @author Søren Birk <soer9648@eucl.dk>
  */
-/** Translations */
 $lang['tpl_dir']               = 'Undermappe for skabelonerne i mediemanageren';
+$lang['odt_template']          = 'Skabelon til import at stile (ODT eller CSS fil)';
 $lang['showexportbutton']      = 'Vis ODT Eksportknap';
+$lang['css_usage']             = 'Anvend importeret CSS på ODT stile?';
+$lang['media_sel']             = 'Hvilken @media selector skal benyttes til at hente CSS egenskaber?';
+$lang['css_font_size']         = 'CSS basis skriftstørrelse (definerer 1em)';
 $lang['css_template']          = 'Hvilken skabelon skal benyttes til formatering af ODT\'erne?';
+$lang['apply_fs_to_non_css']   = 'Anvend CSS skriftstørrelse til ODT skabelon og standard stile i udvidelse?';
+$lang['twips_per_pixel_x']     = 'Twip per pixel (X-akse)';
+$lang['twips_per_pixel_y']     = 'Twip per pixel (Y-akse)';
 $lang['format']                = 'Sideformat';
 $lang['orientation']           = 'Sideretning';
 $lang['margin_top']            = 'Øverste sidemargen i cm';
@@ -22,3 +31,7 @@ $lang['toc_maxlevel']          = 'Maksimale niveau for indholdsfortegnelse';
 $lang['toc_leader_sign']       = 'Fyldtegn for indholdsfortegnelse';
 $lang['toc_indents']           = 'Indholdsfortegnelse: Indrykning pr. niveau i cm';
 $lang['toc_pagebreak']         = 'Indsæt et sideskift efter indholdsfortegnelsen?';
+$lang['toc_style']             = 'Tekststil til indholdsfortegnelse';
+$lang['index_in_browser']      = 'Vis pladsholder til indholdsfortegnelse/indeks i browser?';
+$lang['outline_list_style']    = 'Omrids stil';
+$lang['convert_to_pdf']        = 'Kommando der skal køres for at konvertere PDF';

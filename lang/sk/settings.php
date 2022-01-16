7eb85131bd3172b62870c9690011a84bdafae11d
Viktor Kristian <vkristian@gmail.com>
2022-01-16 22:15:45 +0100

    added translation to sk language

---
+++ lang/sk/settings.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * Slovak language file for config settings.
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Viktor Kristian <vkristian@gmail.com>
+ * @package    DokuWiki\lang\en\settings
+ */
+
+/**
+ * @global array $lang['tpl_dir'] Translation for 'template directory'
+ */
+$lang['tpl_dir'] = 'Podadresár so šablónami v správe médií';
+
+/** @var array $lang['tpl_dir'] Translation for 'ODT template file (ODT or CSS file)' */
+$lang['odt_template'] = 'Šablóna pre inport štýlov (ODT alebo CSS súbor)';
+$lang['showexportbutton'] = 'Zobrazovať tlačidlo "Export do ODT"';
+$lang['showpdfexportbutton'] = 'Zobrazovať tlačidlo "Export ODT=>PDF"';
+
+$lang['css_usage'] = 'Aplikovať importované CSS do ODT štýlov?';
+$lang['media_sel'] = 'Ktorý @media selector má byť použitý na načítanie CSS vlastností?';
+$lang['css_font_size'] = 'Hodnota CSS base font size (definujúca 1em)';
+$lang['css_template'] = 'Ktorá šablóna má byť použitá na formátovanie ODT súborov?';
+$lang['apply_fs_to_non_css'] = 'Aplikovať CSS font size na ODT šablóny a implicitné štýly pluginov?';
+
+$lang['twips_per_pixel_x'] = 'Twipov na pixel (na osi X)';
+$lang['twips_per_pixel_y'] = 'Twipov na pixel (na osi Y)';
+
+$lang['format']                = 'Formát stránky';
+$lang['orientation']           = 'Orientácia stránky';
+$lang['margin_top']            = 'Horný okraj stránky v cm';
+$lang['margin_right']          = 'Pravý okraj stránky v cm';
+$lang['margin_bottom']         = 'Spodný okraj stránky v cm';
+$lang['margin_left']           = 'Ľavý okraj stránky v cm';
+
+$lang['disable_links']         = 'Zakázať vytváranie odkazov?';
+
+$lang['toc_maxlevel']          = 'Maximálna hĺbka obsahu';
+$lang['toc_leader_sign']       = 'Vodiaci znak obsahu';
+$lang['toc_indents']           = 'Obsah: odsadenie úrovní v cm';
+$lang['toc_pagebreak']         = 'Vložiť zalomenie stránky za obsah?';
+$lang['toc_style']             = 'Štýl textu obsahu';
+$lang['index_in_browser']      = 'Zobraziť zástupný znak pre obsah/index v prehliadači?';
+
+$lang['outline_list_style']    = 'Štýl osnovy';
+$lang['olist_label_align']     = 'Zarovnanie čísel pred zoznamom prvkov (napr. čísla kapitol)';
+$lang['convert_to_pdf']        = 'Príkaz na spustenie konverzie do formátu PDF';

847143f0b84eb864f0654043da7124801fca87e2
RandyChen <zzxxccvv484632@gmail.com>
2022-10-25 17:50:05 +0200

    translation update

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -3,14 +3,26 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
+ * @author RandyChen <zzxxccvv484632@gmail.com>
  * @author RainSlide <RainSlide@outlook.com>
  * @author Wen Qixiang <wen@bootingman.org>
  * @author 2576562185 <2576562185@qq.com>
  */
 $lang['encoding']              = 'UTF-8';
-$lang['view']                  = '将页面以 Open Document 格式导出';
-$lang['export_odt_button']     = '以ODT格式导出';
-$lang['export_odt_pdf_button'] = 'ODT=>PDF 导出';
-$lang['needtitle']             = '请填写标题。';
-$lang['needns']                = '请提供一个已经存在的命名空间。';
-$lang['empty']                 = '您还没有选择页面。';
+$lang['direction']             = '由右至左';
+$lang['view']                  = '將頁面以Open Document格式輸出';
+$lang['export_odt_button']     = '輸出ODT檔';
+$lang['export_odt_pdf_button'] = '輸出PDF檔';
+$lang['tpl_not_found']         = '錯誤：在模板目錄「“%s”」中找不到 ODT 模板「“%s”」。輸出已中止。';
+$lang['toc_title']             = '目錄';
+$lang['chapter_title']         = '章節索引';
+$lang['toc_msg']               = '將在此處插入目錄';
+$lang['chapter_msg']           = '將在此處插入章節索引';
+$lang['update_toc_msg']        = '請記得在輸出後更新目錄';
+$lang['update_chapter_msg']    = '請記得在輸出後更新章節索引';
+$lang['needtitle']             = '請提供標題';
+$lang['needns']                = '請提供現有的命名空間';
+$lang['empty']                 = '您沒有選擇頁面';
+$lang['forbidden']             = '您無權訪問頁面：「%s」。<br/><br/>請使用“跳過禁止​​訪問的頁面”選項來創建包含可用頁面的頁面。';
+$lang['conversion_failed_msg'] = '====== 轉換 ODT 文檔時出錯：====== 執行的命令行：<code>%command%</code> 錯誤代碼：%errorcode% 錯誤消息：<code>%errormessage %</code> [[%pageid%|返回上一頁]]';
+$lang['init_failed_msg']       = '====== ODT 文檔初始化時出錯：====== 你的 DokuWiki 版本是否兼容 ODT 插件？自 2017-02-11 發布以來，ODT 插件需要 DokuWiki 版本“Detritus”或更新版本！有關詳細要求信息，請參閱 DokuWiki.org 上 ODT 插件頁面的 [[https://www.dokuwiki.org/plugin:odt#requirements|要求部分]]。（您的 DokuWiki 版本是 %DWVERSION%）';

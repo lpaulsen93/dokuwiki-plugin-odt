7ad14133a541946042ab00f2ea523b34fa8c17e2
Neuro IQ Gummies <edmundstanfill@t-online.de>
2022-11-06 22:35:05 +0100

    translation update

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,63 +1,29 @@
 <?php
+
 /**
  * English language file.
- * 
+ *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  * @package DokuWiki\lang\en\lang
+ *
+ * @author Neuro IQ Gummies <edmundstanfill@t-online.de>
  */
-
-// settings must be present and set appropriately for the language
-$lang['encoding']   = 'utf-8';
-$lang['direction']  = 'ltr';
-
-// export button
-$lang['view'] = 'Export page to Open Document format';
-$lang['export_odt_button'] = 'ODT export';
-$lang['export_odt_pdf_button'] = 'ODT=>PDF export';
-
-// template not found in the directory
-$lang['tpl_not_found'] = 'ERROR : the ODT template "%s" was not found in the templates directory "%s". The export has been aborted.';
-
-// default TOC and chapter index title
-$lang['toc_title']          = 'Table of Contents';
-$lang['chapter_title']      = 'Chapter Index';
-$lang['toc_msg']            = 'A Table of Contents will be inserted here.';
-$lang['chapter_msg']        = 'A Chapter Index will be inserted here.';
-$lang['update_toc_msg']     = 'Please remember to update the Table Of Contents after export.';
-$lang['update_chapter_msg'] = 'Please remember to update the Chapter Index after export.';
-
-$lang['needtitle']         = 'Please provide a title.';
-$lang['needns']            = 'Please provide an existing namespace.';
-$lang['empty']             = "You don't have pages selected yet.";
-$lang['forbidden']         = "You have no access to these pages: %s.<br/><br/>Use option 'Skip Forbidden Pages' to create your book with the available pages.";
-
-// Error message for failed conversion.
-// The following replacments are supported:
-// %command%      = the complete command line which was executed
-// %errorcode%    = the error code reported after executing the command
-// %errormessage% = the detailed error message reported after executing the command
-$lang['conversion_failed_msg'] =
-'====== An error occured during conversion of the ODT document: ======
-
-Executed command line:
-
-<code>%command%</code>
-
-Error code: %errorcode%
-
-Error message:
-
-<code>%errormessage%</code>
-
-[[%pageid%|Back to previous page]]';
-
-// Error message for failed conversion.
-$lang['init_failed_msg'] =
-'====== An error occured during initialization of the ODT document: ======
-
-Is your DokuWiki version compatible to the ODT plugin?
-
-Since release 2017-02-11 the ODT plugin requires DokuWiki release “Detritus” or newer!
-For detailed requirements information please see the [[https://www.dokuwiki.org/plugin:odt#requirements|Requirements section]] of the ODT plugin page at DokuWiki.org.
-
-(Your DokuWiki release is %DWVERSION%)';
+$lang['encoding']              = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['direction']             = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['view']                  = 'Wagerup';
+$lang['export_odt_button']     = 'Wagerup';
+$lang['export_odt_pdf_button'] = 'Wagerup';
+$lang['tpl_not_found']         = 'Wagerup';
+$lang['toc_title']             = 'Just want to say Hello!';
+$lang['chapter_title']         = 'Just want to say Hello!';
+$lang['toc_msg']               = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['chapter_msg']           = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['update_toc_msg']        = 'Wagerup';
+$lang['update_chapter_msg']    = 'Wagerup';
+$lang['needtitle']             = 'Just want to say Hello!';
+$lang['needns']                = 'Stanfill';
+$lang['empty']                 = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['forbidden']             = 'GodFather';
+$lang['conversion_failed_msg'] = 'Sweet website, super design, very clean and 
+employ pleasant.';
+$lang['init_failed_msg']       = 'https://dekatrian.com/index.php/User:HiltonTrumble2';
--- lang/en/settings.php
+++ lang/en/settings.php
@@ -1,46 +1,40 @@
 <?php
+
 /**
  * English language file for config settings.
  *
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * @author     Aurelien Bompard <aurelien@bompard.org>
  * @package    DokuWiki\lang\en\settings
- */
-
-/**
+ *
  * @global array $lang['tpl_dir'] Translation for 'template directory'
+ * @var array $lang['tpl_dir'] Translation for 'ODT template file (ODT or CSS file)' 
+ *
+ * @author Neuro IQ Gummies <edmundstanfill@t-online.de>
+ * @author Aurelien Bompard <aurelien@bompard.org>
  */
-$lang['tpl_dir'] = 'Sub-directory for the templates in the media manager';
-
-/** @var array $lang['tpl_dir'] Translation for 'ODT template file (ODT or CSS file)' */
-$lang['odt_template'] = 'Template for style import (ODT or CSS file)';
-$lang['showexportbutton'] = 'Display ODT Export Button';
-
-$lang['css_usage'] = 'Apply imported CSS to ODT styles?';
-$lang['media_sel'] = 'Which @media selector shall be used to query CSS properties?';
-$lang['css_font_size'] = 'CSS base font size (defining 1em)';
-$lang['css_template'] = 'Which template should be used for formatting the ODT files?';
-$lang['apply_fs_to_non_css'] = 'Apply CSS font size to ODT template and plugin default styles?';
-
-$lang['twips_per_pixel_x'] = 'Twips per pixel (X axis)';
-$lang['twips_per_pixel_y'] = 'Twips per pixel (Y axis)';
-
-$lang['format']                = 'Page format';
-$lang['orientation']           = 'Page orientation';
-$lang['margin_top']            = 'Page margin top in cm';
-$lang['margin_right']          = 'Page margin right in cm';
-$lang['margin_bottom']         = 'Page margin bottom in cm';
-$lang['margin_left']           = 'Page margin left in cm';
-
-$lang['disable_links']         = 'Disable link creation?';
-
-$lang['toc_maxlevel']          = 'Maximum depth of the table of contents';
-$lang['toc_leader_sign']       = 'Leader sign for the table of contents';
-$lang['toc_indents']           = 'Table of contents: Indentation per level in cm';
-$lang['toc_pagebreak']         = 'Insert a pagebreak after the table of contents?';
-$lang['toc_style']             = 'Text style for table of contents';
-$lang['index_in_browser']      = 'Display placeholder for table of contents/index in the browser?';
-
-$lang['outline_list_style']    = 'Outline style';
-
-$lang['convert_to_pdf']        = 'Command to run for PDF conversion';
+$lang['tpl_dir']               = 'Just want to say Hello!';
+$lang['odt_template']          = 'Wagerup';
+$lang['showexportbutton']      = 'Wagerup';
+$lang['css_usage']             = 'Wagerup';
+$lang['media_sel']             = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['css_font_size']         = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['css_template']          = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['apply_fs_to_non_css']   = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['twips_per_pixel_x']     = 'Sweet website, super design, very clean and employ 
+pleasant.';
+$lang['twips_per_pixel_y']     = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['format']                = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['orientation']           = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['margin_top']            = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['margin_right']          = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['margin_bottom']         = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['margin_left']           = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['disable_links']         = 'https://dekatrian.com/index.php/User:HiltonTrumble2';
+$lang['toc_maxlevel']          = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['toc_leader_sign']       = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['toc_indents']           = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['toc_pagebreak']         = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['toc_style']             = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['index_in_browser']      = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['outline_list_style']    = 'Sweet website, super design, very clean and employ pleasant.';
+$lang['convert_to_pdf']        = 'Sweet website, super design, very clean and employ pleasant.';

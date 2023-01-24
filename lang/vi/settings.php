0ffa0d7ba193689fd61d45fdb69ca307c472350a
Minh <phandinhminh@protonmail.ch>
2023-01-24 23:35:05 +0100

    translation update

---
+++ lang/vi/settings.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ *
+ * @author Minh <phandinhminh@protonmail.ch>
+ */
+$lang['tpl_dir']               = 'Thư mục phụ cho mẫu template trong trình quản lý media';
+$lang['odt_template']          = 'Mẫu template cho nhập kiểu style (file tệp tin ODT hoặc CSS)';
+$lang['showexportbutton']      = 'Hiện nút xuất ODT ';
+$lang['css_usage']             = 'Áp dụng CSS đã nhập vào các kiểu style ODT?';
+$lang['media_sel']             = 'Bộ chọn @media nào sẽ được sử dụng để truy vấn các thuộc tính CSS?';
+$lang['css_font_size']         = 'Kích thước font cơ sở CSS (xác định 1em)';
+$lang['css_template']          = 'Mẫu template nào nên được sử dụng để định dạng tệp ODT?';
+$lang['apply_fs_to_non_css']   = 'Áp dụng kích thước font CSS cho mẫu template ODT và kiểu style mặc định của tiện ích plugin?';
+$lang['twips_per_pixel_x']     = 'Số lần gấp đôi trên mỗi pixel (trục X)';
+$lang['twips_per_pixel_y']     = 'Số lần gấp đôi trên mỗi pixel (trục Y)';
+$lang['format']                = 'Định dạng trang';
+$lang['orientation']           = 'Định hướng trang';
+$lang['margin_top']            = 'Lề trang trên tính bằng cm';
+$lang['margin_right']          = 'Lề trang phải tính bằng cm';
+$lang['margin_bottom']         = 'Lề trang đáy tính bằng cm';
+$lang['margin_left']           = 'Lề trang trái tính bằng cm';
+$lang['disable_links']         = 'Tắt tính năng tạo liên kết?';
+$lang['toc_maxlevel']          = 'Độ sâu tối đa của mục lục';
+$lang['toc_leader_sign']       = 'Dấu sign hàng đầu cho mục lục';
+$lang['toc_indents']           = 'Mục lục: Thụt lề mỗi cấp tính bằng cm';
+$lang['toc_pagebreak']         = 'Chèn ngắt trang sau mục lục?';
+$lang['toc_style']             = 'Kiểu chữ cho mục lục';
+$lang['index_in_browser']      = 'Hiển thị trình giữ chỗ cho mục lục/chỉ mục trong trình duyệt?';
+$lang['outline_list_style']    = 'Kiểu style phác thảo';
+$lang['convert_to_pdf']        = 'Lệnh chạy để chuyển đổi PDF';

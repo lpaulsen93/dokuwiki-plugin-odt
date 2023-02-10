05704f0894262502dc198ce710ad577889733eba
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2023-02-09 21:40:07 -0300

    Create settings.php

---
+++ lang/pt-br/settings.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * English language file for config settings.
+ *
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * @author     Aurelien Bompard <aurelien@bompard.org>
+ * @package    DokuWiki\lang\en\settings
+ */
+
+/**
+ * @global array $lang['tpl_dir'] Translation for 'template directory'
+ */
+$lang['tpl_dir'] = 'Sub-directório para templates no gerenciador de mídias';
+
+/** @var array $lang['tpl_dir'] Translation for 'ODT template file (ODT or CSS file)' */
+$lang['odt_template'] = 'Template para importação de estilos (arquivo CSS ou ODT)';
+$lang['showexportbutton'] = 'Exibir botão para Exportar para ODT';
+
+$lang['css_usage'] = 'Aplicar estilos CSS e ODT importados?';
+$lang['media_sel'] = 'Qual seletor @media será usado para consultar propriedades CSS?';
+$lang['css_font_size'] = 'Tamanho de fonte CSS base (definindo 1em)';
+$lang['css_template'] = 'Qual template deverá ser usado para formatar os arquivos ODT?';
+$lang['apply_fs_to_non_css'] = 'Aplicar tamanho de fonte CSS para os estilos padrões do template CSS e ODT?';
+
+$lang['twips_per_pixel_x'] = 'Twips por pixel (eixo X)';
+$lang['twips_per_pixel_y'] = 'Twips por pixel (eixo Y)';
+
+$lang['format']                = 'Formato da página';
+$lang['orientation']           = 'Orientação da página';
+$lang['margin_top']            = 'Margem superior da página em cm';
+$lang['margin_right']          = 'Margem direita da página em cm';
+$lang['margin_bottom']         = 'Margem inferior da página em cm';
+$lang['margin_left']           = 'Margem esquerda da página em cm';
+
+$lang['disable_links']         = 'Desabilitar criação de link?';
+
+$lang['toc_maxlevel']          = 'Profundidade máxima do sumário';
+$lang['toc_leader_sign']       = 'Caractere de preenchimento do sumário';
+$lang['toc_indents']           = 'Sumário: Indentação por nível em cm';
+$lang['toc_pagebreak']         = 'Inserir uma quebra de página após o sumário?';
+$lang['toc_style']             = 'Text style for table of contents';
+$lang['index_in_browser']      = 'Exibir placeholder para o sumário/índice no navegador?';
+
+$lang['outline_list_style']    = 'Estilo de contorno';
+
+$lang['convert_to_pdf']        = 'Comando a executar para conversão para PDF';

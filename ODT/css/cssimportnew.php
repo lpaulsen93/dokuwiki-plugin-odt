198daa152dfad1cce5230d44acbab4a7335dff37
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2023-05-04 22:55:17 -0300

    Fix GeSHi and Wrap CSS rendering

--- ODT/css/cssimportnew.php
+++ ODT/css/cssimportnew.php
@@ -1105,7 +1105,7 @@ class cssimportnew {
                 // Only accept a property value if the current specificity of the matched
                 // rule/selector is higher or equal than the highest one.
                 foreach ($current as $property => $value) {
-                    if (isset($highest [$property]) && $specificity >= $highest [$property]) {
+                    if ($specificity >= $highest [$property]) {
                         $highest [$property] = $specificity;
                         $temp [$property] = $value;
                     }

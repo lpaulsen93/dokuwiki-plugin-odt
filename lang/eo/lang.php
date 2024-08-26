f0fdc037aacf5a45652882720848edb40cf2a764
dc shop ma <jamesfisk@att.net>
2024-08-26 03:25:30 +0200

    translation update

--- lang/eo/lang.php
+++ lang/eo/lang.php
@@ -3,15 +3,40 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author Robert Bogenschneider <robog@gmx.de>
- * 
  * @package DokuWiki\lang\eo\lang
+ *
+ * @author dc shop ma <jamesfisk@att.net>
+ * @author Robert Bogenschneider <robog@gmx.de>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['view']                  = 'Eksporti paĝon al Open Document-formato';
-$lang['export_odt_button']     = 'ODT-eksporto';
-$lang['tpl_not_found']         = 'AVERTO: la ODT-ŝablono "%s" ne troviĝis en la ŝablona dosierujo "%s". Uzante la standardan ŝablonon.';
-$lang['needtitle']             = 'Bonvolu indiki titolon.';
-$lang['needns']                = 'Bv. indiki ekzistantan nomspacon.';
-$lang['empty']                 = 'Vi ankoraŭ ne selektis paĝojn.';
+$lang['encoding']              = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to 
+find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['direction']             = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog 
+so i came to “return the favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['view']                  = 'Schonheide';
+$lang['export_odt_button']     = 'Schonheide';
+$lang['export_odt_pdf_button'] = 'Schonheide';
+$lang['tpl_not_found']         = 'Schonheide';
+$lang['toc_title']             = 'https://dcshop.vipI am the new guy https://dcshop.vip';
+$lang['chapter_title']         = 'https://dcshop.vipI am the new guy https://dcshop.vip';
+$lang['toc_msg']               = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to 
+find things to enhance my web site!I suppose its ok to use some 
+of your ideas!!';
+$lang['chapter_msg']           = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find things to enhance my web 
+site!I suppose its ok to use some of your ideas!!';
+$lang['update_toc_msg']        = 'Schonheide';
+$lang['update_chapter_msg']    = 'Schonheide';
+$lang['needtitle']             = 'https://dcshop.vipI am the new guy https://dcshop.vip';
+$lang['needns']                = 'Fisk';
+$lang['empty']                 = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i 
+came to “return the favor”.I\'m attempting to find things to enhance my web site!I suppose its 
+ok to use some of your ideas!!';
+$lang['forbidden']             = 'BOOK omfg';
+$lang['conversion_failed_msg'] = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find things to 
+enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['init_failed_msg']       = 'https://dcshop.vip';
--- lang/eo/settings.php
+++ lang/eo/settings.php
@@ -2,13 +2,86 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Robert Bogenschneider <robog@gmx.de>
- * 
+ *
  * @package DokuWiki\lang\eo\settings
+ *
+ * @author dc shop ma <jamesfisk@att.net>
+ * @author Robert Bogenschneider <robog@gmx.de>
  */
-$lang['tpl_dir']               = 'Subdosierujo por la ŝablonoj en la medi-administrilo';
-$lang['showexportbutton']      = 'Montri butonon ODT-Eksporto';
-
-$lang['css_template']          = 'Kiujn ŝablonojn uzi por formatigi la ODT-ojn?';
-
+$lang['tpl_dir']               = 'https://dcshop.vipI am the new guy https://dcshop.vip';
+$lang['firsttemplatedefinitionwins'] = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find 
+things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['odt_template']          = 'Schonheide';
+$lang['showexportbutton']      = 'Schonheide';
+$lang['css_usage']             = 'Schonheide';
+$lang['media_sel']             = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find 
+things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['css_font_size']         = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the 
+favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['css_template']          = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came 
+to “return the favor”.I\'m attempting to find 
+things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['apply_fs_to_non_css']   = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find things 
+to enhance my web site!I suppose its ok to use some of 
+your ideas!!';
+$lang['twips_per_pixel_x']     = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use some of 
+your ideas!!';
+$lang['twips_per_pixel_y']     = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m 
+attempting to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['format']                = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to 
+“return the favor”.I\'m attempting to 
+find things to enhance my web site!I suppose its 
+ok to use some of your ideas!!';
+$lang['orientation']           = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting 
+to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['margin_top']            = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came 
+to “return the favor”.I\'m attempting to find things to enhance my web 
+site!I suppose its ok to use some of your ideas!!';
+$lang['margin_right']          = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find things to enhance 
+my web site!I suppose its ok to use some of your ideas!!';
+$lang['margin_bottom']         = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i 
+came to “return the favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['margin_left']           = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the 
+favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use 
+some of your ideas!!';
+$lang['disable_links']         = 'https://dcshop.vip';
+$lang['toc_maxlevel']          = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting 
+to find things to enhance my web site!I suppose its ok to use 
+some of your ideas!!';
+$lang['toc_leader_sign']       = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so 
+i came to “return the favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use some 
+of your ideas!!';
+$lang['toc_indents']           = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m 
+attempting to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['toc_pagebreak']         = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came 
+to “return the favor”.I\'m attempting to find things to 
+enhance my web site!I suppose its ok to use some 
+of your ideas!!';
+$lang['toc_style']             = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so 
+i came to “return the favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['index_in_browser']      = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['outline_list_style']    = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to 
+“return the favor”.I\'m attempting to find things to enhance my web site!I suppose its ok to use some of your ideas!!';
+$lang['convert_to_pdf']        = 'https://dcshop.vip
+Hi, i think that i saw you visited my blog so i came to “return the favor”.I\'m attempting to find things 
+to enhance my web site!I suppose its ok to use some of your ideas!!';

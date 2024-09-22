5b751590ed5600d5bc7d0b974220939bcac17fa5
tastysylvia <masonnies@rambler.ru>
2024-09-22 03:35:43 +0200

    translation update

--- lang/eo/lang.php
+++ lang/eo/lang.php
@@ -3,15 +3,27 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author Robert Bogenschneider <robog@gmx.de>
- * 
  * @package DokuWiki\lang\eo\lang
+ *
+ * @author tastysylvia <masonnies@rambler.ru>
+ * @author Robert Bogenschneider <robog@gmx.de>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['view']                  = 'Eksporti paĝon al Open Document-formato';
-$lang['export_odt_button']     = 'ODT-eksporto';
-$lang['tpl_not_found']         = 'AVERTO: la ODT-ŝablono "%s" ne troviĝis en la ŝablona dosierujo "%s". Uzante la standardan ŝablonon.';
-$lang['needtitle']             = 'Bonvolu indiki titolon.';
-$lang['needns']                = 'Bv. indiki ekzistantan nomspacon.';
-$lang['empty']                 = 'Vi ankoraŭ ne selektis paĝojn.';
+$lang['encoding']              = 'Endnu et år er gået, og vi har sagt farvel til 2016!';
+$lang['direction']             = 'Endnu et år er gået, og vi har sagt farvel til 2016!';
+$lang['view']                  = 'Colney';
+$lang['export_odt_button']     = 'Colney';
+$lang['export_odt_pdf_button'] = 'Colney';
+$lang['tpl_not_found']         = 'Colney';
+$lang['toc_title']             = '17 Confessions That Will Change Your Perception Of Webcam Girls';
+$lang['chapter_title']         = '17 Confessions That Will Change Your Perception Of Webcam Girls';
+$lang['toc_msg']               = 'Endnu et år er gået, og vi har sagt farvel til 2016!';
+$lang['chapter_msg']           = 'Endnu et år er gået, og vi har sagt farvel til 2016!';
+$lang['update_toc_msg']        = 'Colney';
+$lang['update_chapter_msg']    = 'Colney';
+$lang['needtitle']             = '17 Confessions That Will Change Your Perception Of Webcam Girls';
+$lang['needns']                = 'Nies';
+$lang['empty']                 = 'Endnu et år er gået, og vi har sagt farvel til 
+2016!';
+$lang['forbidden']             = 'BOOK omfg';
+$lang['conversion_failed_msg'] = 'Endnu et år er gået, og vi har sagt farvel til 2016!';
+$lang['init_failed_msg']       = 'https://www.sexcams.ai/tastysylvia';

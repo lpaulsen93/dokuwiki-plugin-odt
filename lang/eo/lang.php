2fef311136f93f1fb96e33c140ea92c040338437
Strip Hair Transplant surgery in Hyderabad <bernardovandiver@yahoo.com>
2024-11-29 18:00:37 +0100

    translation update

--- lang/eo/lang.php
+++ lang/eo/lang.php
@@ -3,15 +3,32 @@
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
  *
- * @author Robert Bogenschneider <robog@gmx.de>
- * 
  * @package DokuWiki\lang\eo\lang
+ *
+ * @author Strip Hair Transplant surgery in Hyderabad <bernardovandiver@yahoo.com>
+ * @author Robert Bogenschneider <robog@gmx.de>
  */
-$lang['encoding']              = 'utf-8';
-$lang['direction']             = 'ltr';
-$lang['view']                  = 'Eksporti paĝon al Open Document-formato';
-$lang['export_odt_button']     = 'ODT-eksporto';
-$lang['tpl_not_found']         = 'AVERTO: la ODT-ŝablono "%s" ne troviĝis en la ŝablona dosierujo "%s". Uzante la standardan ŝablonon.';
-$lang['needtitle']             = 'Bonvolu indiki titolon.';
-$lang['needns']                = 'Bv. indiki ekzistantan nomspacon.';
-$lang['empty']                 = 'Vi ankoraŭ ne selektis paĝojn.';
+$lang['encoding']              = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i am going to 
+deliver in school.';
+$lang['direction']             = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i am going to 
+deliver in school.';
+$lang['view']                  = 'Mespelbrunn';
+$lang['export_odt_button']     = 'Mespelbrunn';
+$lang['export_odt_pdf_button'] = 'Mespelbrunn';
+$lang['tpl_not_found']         = 'Mespelbrunn';
+$lang['toc_title']             = 'Im happy I now registeredhttps://www.instagram.com/';
+$lang['chapter_title']         = 'Im happy I now registeredhttps://www.instagram.com/';
+$lang['toc_msg']               = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my 
+presentation subject, which i am going to deliver in school.';
+$lang['chapter_msg']           = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i am going to deliver in school.';
+$lang['update_toc_msg']        = 'Mespelbrunn';
+$lang['update_chapter_msg']    = 'Mespelbrunn';
+$lang['needtitle']             = 'Im happy I now registeredhttps://www.instagram.com/';
+$lang['needns']                = 'Vandiver';
+$lang['empty']                 = 'Excellent way of explaining, and nice piece of writing to 
+obtain data concerning my presentation subject, which i am going 
+to deliver in school.';
+$lang['forbidden']             = 'Stupid white men';
+$lang['conversion_failed_msg'] = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my 
+presentation subject, which i am going to deliver in school.';
+$lang['init_failed_msg']       = 'https://www.instagram.com/';
--- lang/eo/settings.php
+++ lang/eo/settings.php
@@ -2,13 +2,61 @@
 
 /**
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- * 
- * @author Robert Bogenschneider <robog@gmx.de>
- * 
+ *
  * @package DokuWiki\lang\eo\settings
+ *
+ * @author Strip Hair Transplant surgery in Hyderabad <bernardovandiver@yahoo.com>
+ * @author Robert Bogenschneider <robog@gmx.de>
  */
-$lang['tpl_dir']               = 'Subdosierujo por la ŝablonoj en la medi-administrilo';
-$lang['showexportbutton']      = 'Montri butonon ODT-Eksporto';
-
-$lang['css_template']          = 'Kiujn ŝablonojn uzi por formatigi la ODT-ojn?';
-
+$lang['tpl_dir']               = 'Im happy I now registeredhttps://www.instagram.com/';
+$lang['firsttemplatedefinitionwins'] = 'Excellent way of explaining, and nice piece of writing to 
+obtain data concerning my presentation subject, which i am going to deliver in school.';
+$lang['odt_template']          = 'Mespelbrunn';
+$lang['showexportbutton']      = 'Mespelbrunn';
+$lang['css_usage']             = 'Mespelbrunn';
+$lang['media_sel']             = 'Excellent way of explaining, and nice piece of writing to 
+obtain data concerning my presentation subject, which i am going to deliver 
+in school.';
+$lang['css_font_size']         = 'Excellent way of explaining, and nice piece of writing to 
+obtain data concerning my presentation subject, which i am 
+going to deliver in school.';
+$lang['css_template']          = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which 
+i am going to deliver in school.';
+$lang['apply_fs_to_non_css']   = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i am going to 
+deliver in school.';
+$lang['twips_per_pixel_x']     = 'Excellent way of explaining, and nice piece of writing 
+to obtain data concerning my presentation subject,
+which i am going to deliver in school.';
+$lang['twips_per_pixel_y']     = 'Excellent way of explaining, and nice piece of writing to 
+obtain data concerning my presentation subject, which i 
+am going to deliver in school.';
+$lang['format']                = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject,
+which i am going to deliver in school.';
+$lang['orientation']           = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my 
+presentation subject, which i am going to deliver in school.';
+$lang['margin_top']            = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i am going to 
+deliver in school.';
+$lang['margin_right']          = 'Excellent way of explaining, and nice piece of writing to 
+obtain data concerning my presentation subject, which 
+i am going to deliver in school.';
+$lang['margin_bottom']         = 'Excellent way of explaining, and nice piece of writing to 
+obtain data concerning my presentation subject, which i am going to deliver 
+in school.';
+$lang['margin_left']           = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i am going 
+to deliver in school.';
+$lang['disable_links']         = 'https://www.instagram.com/';
+$lang['toc_maxlevel']          = 'Excellent way of explaining, and nice piece of writing to obtain data concerning 
+my presentation subject, which i am going to deliver in school.';
+$lang['toc_leader_sign']       = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i 
+am going to deliver in school.';
+$lang['toc_indents']           = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i am going to deliver in school.';
+$lang['toc_pagebreak']         = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my 
+presentation subject, which i am going to deliver in school.';
+$lang['toc_style']             = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject, which i am going to deliver in school.';
+$lang['index_in_browser']      = 'Excellent way of explaining, and nice piece of writing to 
+obtain data concerning my presentation subject, which i am going to deliver in school.';
+$lang['outline_list_style']    = 'Excellent way of explaining, and nice piece of writing to obtain data concerning my presentation subject,
+which i am going to deliver in school.';
+$lang['convert_to_pdf']        = 'Excellent way of explaining, and nice piece of writing 
+to obtain data concerning my presentation subject, which i am going to 
+deliver in school.';
